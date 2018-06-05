86a193ffac8bb065a27b2df74975766e5443ebaa
Emory Merryman <emory.merryman@gmail.com>
2018-06-05 16:53:52 +0000

    fix it

--- install-docker-lvm-plugin.sh
+++ install-docker-lvm-plugin.sh
@@ -1,7 +1,6 @@
 #!/bin/sh
 
-sudo chmod 0600 /etc/cron.hourly/dnf-update.sh &&
-    sudo dnf install --assumeyes golang git golang-github-cpugyu83-go-md2man &&
+sudo dnf install --assumeyes golang git golang-github-cpugyu83-go-md2man &&
     git clone git@github.com:shishir-a412ed/docker-lvm-plugin.git &&
     cd docker-lvm-plugin &&
     make &&
