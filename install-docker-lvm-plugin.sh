dbb199a00e5bb5197f35790b9a3bc6e80395413d
Emory Merryman <emory.merryman@gmail.com>
2018-06-05 18:41:12 +0000

    try this

--- install-docker-lvm-plugin.sh
+++ install-docker-lvm-plugin.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
 
 sudo dnf install --assumeyes golang git golang-github-cpuguy83-go-md2man &&
-    git clone git@github.com:shishir-a412ed/docker-lvm-plugin.git &&
+    git clone https://github.com/projectatomic/docker-lvm-plugin.git &&
     cd docker-lvm-plugin &&
+    go get ./...
     make &&
     make install 
