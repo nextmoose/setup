645187c7f52a6058926e69f8e020114448dd3da8
Emory Merryman <emory.merryman@gmail.com>
2018-06-05 18:48:34 +0000

    go to random dir and use sudo to install

--- install-docker-lvm-plugin.sh
+++ install-docker-lvm-plugin.sh
@@ -1,8 +1,9 @@
 #!/bin/sh
 
 sudo dnf install --assumeyes golang git golang-github-cpuguy83-go-md2man &&
+    cd $(mktemp -d) &&
     git clone https://github.com/projectatomic/docker-lvm-plugin.git &&
     cd docker-lvm-plugin &&
     go get ./...
     make &&
-    make install 
+    sudo make install 
