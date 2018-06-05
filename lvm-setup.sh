b488847838fea062faf732ff5e27bc6ed6d39ad6
Emory Merryman <emory.merryman@gmail.com>
2018-06-05 19:57:46 +0000

    add start docker

--- lvm-setup.sh
+++ lvm-setup.sh
@@ -4,6 +4,7 @@ sudo umount /srv/lvm-plugin/reserve &&
     sudo rm -rf /srv/lvm-plugin/reserve &&
     TEMP_FSTAB=$(mktemp) &&
     sudo grep -v "/srv/lvm-plugin/reserve" /etc/fstab &&
-    cat ${TEMP_FSTAB} > /etc/fstab &&
-    lvcreate --size 10G --thin docker/thin &&
+    cat ${TEMP_FSTAB} | sudo tee /etc/fstab &&
+    sudo lvremove docker/srv_lvm-plugin_reserve &&
+    sudo lvcreate --size 10G --thin docker/thin &&
     true
