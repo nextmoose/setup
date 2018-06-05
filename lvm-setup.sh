c42db8a6ef0aa5ec6e6bf8a7e522495bb17186b9
Emory Merryman <emory.merryman@gmail.com>
2018-06-05 19:44:43 +0000

    fix lvm-setup

--- lvm-setup.sh
+++ lvm-setup.sh
@@ -1,8 +1,9 @@
 #!/bin/sh
 
-sudo umount /var/reserve &&
-    sudo rm -rf /var/reserve &&
+sudo umount /srv/lvm-plugin/reserve &&
+    sudo rm -rf /srv/lvm-plugin/reserve &&
     TEMP_FSTAB=$(mktemp) &&
-    sudo grep -v "/var/reserve" /etc/fstab &&
+    sudo grep -v "/srv/lvm-plugin/reserve" /etc/fstab &&
     cat ${TEMP_FSTAB} > /etc/fstab &&
+    lvcreate --size 10G --thin docker/thin &&
     true
