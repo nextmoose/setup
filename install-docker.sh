02f7ecdae46507c84f84da626fd3f91b342600a8
Emory Merryman <emory.merryman@gmail.com>
2018-06-05 16:46:09 +0000

    adding

---
+++ install-docker.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+sudo dnf --assumeyes install dnf-plugins-core &&
+    sudo \
+        dnf \
+        config-manager \
+        --add-repo https://download.docker.com/linux/fedora/docker-ce.repo \
+        &&
+    sudo dnf install --assumeyes docker-ce &&
+    sudo dnf install --assumeyes docker-ce-18.03.1.ce &&
+    true
