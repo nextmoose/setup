2243e39f2868808be77abacfa703ba68e8f29ba5
Emory Merryman <emory.merryman@gmail.com>
2018-06-05 17:12:16 +0000

    fix typo"

--- install-docker-lvm-plugin.sh
+++ install-docker-lvm-plugin.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-sudo dnf install --assumeyes golang git golang-github-cpugyu83-go-md2man &&
+sudo dnf install --assumeyes golang git golang-github-cpuguy83-go-md2man &&
     git clone git@github.com:shishir-a412ed/docker-lvm-plugin.git &&
     cd docker-lvm-plugin &&
     make &&
