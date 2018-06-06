e6e10d2749e89447ff8fd71ba1d433bb64b25a98
Emory Merryman <emory.merryman@gmail.com>
2018-06-06 14:04:22 +0000

    use sudo when needed

--- start-xhost.sh
+++ start-xhost.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-sed -i "s#^exit 0\$#/usr/bin/xhost +local:#" /etc/gdm/Init/Default &&
+sudo sed -i "s#^exit 0\$#/usr/bin/xhost +local:#" /etc/gdm/Init/Default &&
     echo exit 0 >> /etc/gdm/Init/Default &&
     /usr/bin/xhost +local: &&
     true
