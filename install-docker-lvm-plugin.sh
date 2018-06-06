65c194ac2b141eb1ebbdbb092eb398684a9583e7
Emory Merryman <emory.merryman@gmail.com>
2018-06-06 22:22:56 +0000

    wtf why is this necessary?

--- install-docker-lvm-plugin.sh
+++ install-docker-lvm-plugin.sh
@@ -8,7 +8,6 @@ sudo dnf install --assumeyes golang git golang-github-cpuguy83-go-md2man &&
     git clone https://github.com/projectatomic/docker-lvm-plugin.git &&
     cd docker-lvm-plugin &&
     echo A &&
-    go build &&
     echo B &&
     make &&
     echo C &&
