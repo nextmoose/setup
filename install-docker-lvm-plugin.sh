407bac3da0b0cdd6b5f1726971e7075ee618d528
Emory Merryman <emory.merryman@gmail.com>
2018-06-06 14:02:01 +0000

    remove bad line

--- install-docker-lvm-plugin.sh
+++ install-docker-lvm-plugin.sh
@@ -4,7 +4,6 @@ sudo dnf install --assumeyes golang git golang-github-cpuguy83-go-md2man &&
     cd $(mktemp -d) &&
     git clone https://github.com/projectatomic/docker-lvm-plugin.git &&
     cd docker-lvm-plugin &&
-    go get ./...
     make &&
     sudo make install &&
     sed -i "s#^VOLUME_GROUP=\$#VOLUME_GROUP=docker#" /etc/docker/docker-lvm-plugin
