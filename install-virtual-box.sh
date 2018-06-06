081ff5447a78b5fd51c53b318ecaf4ffb34f1261
Emory Merryman <emory.merryman@gmail.com>
2018-06-06 16:43:21 +0000

    run vboxconfig

--- install-virtual-box.sh
+++ install-virtual-box.sh
@@ -3,4 +3,5 @@
 curl https://download.virtualbox.org/virtualbox/rpm/fedora/virtualbox.repo | sudo tee /etc/yum.repos.d/virtualbox.repo &&
     sudo dnf install --assumeyes kernel* &&
     sudo dnf install --assumeyes VirtualBox-5.2 &&
+    sudo /sbin/vboxconfig &&
     true
