97123a5c3add03efbab21f5f73c5685910d049f0
Emory Merryman <emory.merryman@gmail.com>
2018-06-06 15:37:35 +0000

    add install vb script

--- install-virtual-box.sh
+++ install-virtual-box.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
 curl https://download.virtualbox.org/virtualbox/rpm/fedora/virtualbox.repo | sudo tee /etc/yum.repos.d/virtualbox.repo &&
-    dnf install --assumeyes VirtualBox &&
+    sudo dnf install --assumeyes VirtualBox &&
     true
