6b9c3593f770384db7905df1d086e107bcfd61ca
Emory Merryman <emory.merryman@gmail.com>
2018-06-06 23:11:29 +0000

    just fucking do it 3

--- install-docker-lvm-plugin.sh
+++ install-docker-lvm-plugin.sh
@@ -8,11 +8,10 @@ sudo dnf install --assumeyes golang git golang-github-cpuguy83-go-md2man &&
     export GOPATH=$(mktemp -d) &&
     mkdir ${GOPATH}/src &&
     mkdir ${GOPATH}/src/github.com &&
-    go build &&
-    echo AA &&
-    go get ./... &&
     echo B &&
-    make &&
+    go get ./... &&
     echo C &&
+    make &&
+    echo D &&
     sudo make install &&
     sudo sed -i "s#^VOLUME_GROUP=\$#VOLUME_GROUP=docker#" /etc/docker/docker-lvm-plugin
