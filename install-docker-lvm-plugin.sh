babd708959e6e429d73b9315358e6df02f35e077
Emory Merryman <emory.merryman@gmail.com>
2018-06-06 22:07:08 +0000

    export GOPATH

--- install-docker-lvm-plugin.sh
+++ install-docker-lvm-plugin.sh
@@ -4,6 +4,7 @@ sudo dnf install --assumeyes golang git golang-github-cpuguy83-go-md2man &&
     cd $(mktemp -d) &&
     git clone https://github.com/projectatomic/docker-lvm-plugin.git &&
     cd docker-lvm-plugin &&
+    export GOPATH=$(pwd) &&
     echo Z &&
     go get ./... &&
     echo A &&
