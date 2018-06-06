0248dbc26cc5f4d72e6079f90055d90427975cd9
Emory Merryman <emory.merryman@gmail.com>
2018-06-06 21:57:30 +0000

    use sudo

---
+++ configure.sh
@@ -0,0 +1,28 @@
+#!/bin/sh
+
+sudo docker network create browser &&
+    sudo \
+        docker \
+        docker \
+        container \
+        create \
+        --name browser \
+        --restart always \
+        --privileged \
+        --mount type=tmpfs,destination=/data,readonly=false \
+        --mount type=bind,source=/srv/host/tmp/.X11-unix,destination=/tmp/.X11-unix,readonly=true \
+        --mount type=bind,source=/run/user/${UID}/pulse,destination=/run/user/${UID}/pulse,readonly=false \
+        --mount type=bind,source=/etc/machine-id,destination=/etc/machine-id,readonly=false \
+        --mount type=bind,source=/var/run/dbus/system_bus_socket,destination=/var/run/dbus/system_bus_socket,readonly=false \
+        --mount type=bind,source=/var/lib/dbus,destination=/var/lib/dbus,readonly=false \
+        --mount type=tmpfs,destination=/tmp,readonly=false \
+        --shm-size 256m \
+        --label expiry=$(date --date "now + 1 month" +%s) \
+        --env DISPLAY="${DISPLAY}" \
+        --env TARGET_UID="${UID}" \
+        --env XDG_RUNTIME_DIR=/run/user/${UID} \
+        urgemerge/chromium-pulseaudio@sha256:21d8120ff7857afb0c18d4abf098549de169782e652437441c3c7778a755e46f \
+        &&
+    sudo docker network connect browser browser &&
+    sudo docker container start browser &&
+    true
