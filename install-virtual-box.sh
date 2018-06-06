013958dcd11a24f6a37daa75cdfbbf26d950c060
Emory Merryman <emory.merryman@gmail.com>
2018-06-06 15:38:25 +0000

    is it necessary to update first

--- install-virtual-box.sh
+++ install-virtual-box.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 
 curl https://download.virtualbox.org/virtualbox/rpm/fedora/virtualbox.repo | sudo tee /etc/yum.repos.d/virtualbox.repo &&
+    sudo dnf update --assumeyes &&
     sudo dnf install --assumeyes VirtualBox &&
     true
