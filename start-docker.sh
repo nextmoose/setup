564bfe4878736df8ce54c1564728092f9338d8d0
Emory Merryman <emory.merryman@gmail.com>
2018-06-06 02:58:41 +0000

    start docker-lvm-plugin

--- start-docker.sh
+++ start-docker.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
 
 sudo systemctl enable docker.service &&
-    sudo systemctl start docker.service
+    sudo systemctl enable docker-lvm-plugin &&
+    sudo systemctl start docker.service &&
+    sudo systemctl start docker-lvm-plugin
