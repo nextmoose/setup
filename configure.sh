8d67796afb63ebdfdc4f5753071331710803d454
Emory Merryman <emory.merryman@gmail.com>
2018-06-06 23:58:19 +0000

    fix bindings

--- configure.sh
+++ configure.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 
 sudo docker network create browser &&
+    DATA_DIR=$(mktemp -d) &&
+    TEMP_DIR=$(mktemp -d) &&
     sudo \
         docker \
         container \
@@ -8,13 +10,13 @@ sudo docker network create browser &&
         --name browser \
         --restart always \
         --privileged \
-        --mount type=tmpfs,destination=/data,readonly=false \
+        --mount type=bind,source=${DATA_DIR},destination=/data,readonly=false \
         --mount type=bind,source=/tmp/.X11-unix,destination=/tmp/.X11-unix,readonly=true \
         --mount type=bind,source=/run/user/${UID}/pulse,destination=/run/user/${UID}/pulse,readonly=false \
         --mount type=bind,source=/etc/machine-id,destination=/etc/machine-id,readonly=false \
         --mount type=bind,source=/var/run/dbus/system_bus_socket,destination=/var/run/dbus/system_bus_socket,readonly=false \
         --mount type=bind,source=/var/lib/dbus,destination=/var/lib/dbus,readonly=false \
-        --mount type=tmpfs,destination=/tmp,readonly=false \
+        --mount type=bind,source=${TEMP_DIR},destination=/tmp,readonly=false \
         --shm-size 256m \
         --label expiry=$(date --date "now + 1 month" +%s) \
         --env DISPLAY="${DISPLAY}" \
