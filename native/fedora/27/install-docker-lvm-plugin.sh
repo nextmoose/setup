b21bc07afee80e155f286397556553128a949821
Emory Merryman <emory.merryman@gmail.com>
2018-06-07 15:31:15 +0000

    add sudo tree just so we do not have to reenter password later

--- native/fedora/27/install-docker-lvm-plugin.sh
+++ native/fedora/27/install-docker-lvm-plugin.sh
@@ -7,6 +7,7 @@ sudo dnf install --assumeyes golang git golang-github-cpuguy83-go-md2man &&
     git -C ${GOPATH}/src/github.com clone https://github.com/projectatomic/docker-lvm-plugin.git &&
     cd ${GOPATH}/src/github.com/docker-lvm-plugin &&
     go get ./... &&
+    sudo tree &&
     make &&
     sudo make install &&
     sudo sed -i "s#^VOLUME_GROUP=\$#VOLUME_GROUP=docker#" /etc/docker/docker-lvm-plugin
