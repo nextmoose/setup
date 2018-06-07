e1b2223bf8d22835abb8a0f79ff91a58ad509b34
Emory Merryman <emory.merryman@gmail.com>
2018-06-07 01:40:43 +0000

    install kernel

--- install-virtual-box.sh
+++ install-virtual-box.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 curl https://download.virtualbox.org/virtualbox/rpm/fedora/virtualbox.repo | sudo tee /etc/yum.repos.d/virtualbox.repo &&
-    sudo dnf install --assumeyes kernel* &&
+    sudo dnf install --assumeyes gcc kernel-devel kernel-headers dkms make bzip2 perl kernel-devel-$(uname -r)  &&
     sudo dnf install --assumeyes VirtualBox-5.2 &&
     sudo /sbin/vboxconfig &&
     true
