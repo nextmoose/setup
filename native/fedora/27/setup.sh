2f4e3c0f8944d4b7c3d2e584cd14d6f83d8936c7
Emory Merryman <emory.merryman@gmail.com>
2018-06-07 16:39:51 +0000

    restore

--- native/fedora/27/setup.sh
+++ native/fedora/27/setup.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 
 ./install-docker.sh &&
-    ./install-docker-lvm-plugin.sh &&
+    sudo ./install-docker-lvm-plugin.sh &&
     ./lvm-setup.sh &&
     ./start-docker.sh &&
-#    ./install-dnf-update-cron.sh &&
-#    ./start-xhost.sh &&
-#    ./install-virtual-box.sh &&
-#    sudo dnf update --assumeyes &&
+    ./install-dnf-update-cron.sh &&
+    ./start-xhost.sh &&
+    ./install-virtual-box.sh &&
+    sudo dnf update --assumeyes &&
     true
