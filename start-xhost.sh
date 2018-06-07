28ec821dbe4e5feeecb748ef2ea8e7c087895910
Emory Merryman <emory.merryman@gmail.com>
2018-06-07 01:44:37 +0000

    commit out reboot code

--- start-xhost.sh
+++ start-xhost.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-sudo sed -i "s#^exit 0\$#/usr/bin/xhost +local:#" /etc/gdm/Init/Default &&
-    echo exit 0 | sudo tee --append /etc/gdm/Init/Default &&
+# sudo sed -i "s#^exit 0\$#/usr/bin/xhost +local:#" /etc/gdm/Init/Default &&
+#     echo exit 0 | sudo tee --append /etc/gdm/Init/Default &&
     /usr/bin/xhost +local: &&
     true
