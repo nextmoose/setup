2b1ce5b2b2e586e04533a3874b21b4e3e7998315
Emory Merryman <emory.merryman@gmail.com>
2018-06-07 15:14:40 +0000

    fixed error in lvm-setup.sh

--- native/fedora/27/lvm-setup.sh
+++ native/fedora/27/lvm-setup.sh
@@ -2,8 +2,8 @@
 
 sudo umount /var/reserve &&
     sudo rm -rf /var/reserve &&
-    TEMP_FSTAB=$(mktemp) &&
-    sudo grep -v "/var/reserve" /etc/fstab &&
+    TEMP_FSTAB=$(mktemp $(pwd)/XXXXXXXX) &&
+    sudo grep -v "/var/reserve" /etc/fstab > ${TEMP_FSTAB} &&
     cat ${TEMP_FSTAB} | sudo tee /etc/fstab &&
     sudo lvremove --force docker/var_reserve &&
     true
