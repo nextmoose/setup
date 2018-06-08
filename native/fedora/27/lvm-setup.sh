974404849e7161645aa79c97e48eb9217ed5258d
Emory Merryman <emory.merryman@gmail.com>
2018-06-08 14:42:51 +0000

    add 2 groups

--- native/fedora/27/lvm-setup.sh
+++ native/fedora/27/lvm-setup.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 
-sudo umount /var/reserve &&
+sudo umount /var/reserve/volumes &&
+    sudo umount /var/reserve/system &&
     sudo rm -rf /var/reserve &&
     TEMP_FSTAB=$(mktemp $(pwd)/XXXXXXXX) &&
     sudo grep -v "/var/reserve" /etc/fstab > ${TEMP_FSTAB} &&
