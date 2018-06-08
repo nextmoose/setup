334dbd457c0d26b70d79e10d2d7416e6a0c7221b
Emory Merryman <emory.merryman@gmail.com>
2018-06-08 15:34:23 +0000

    add 2 groups

--- native/fedora/27/lvm-setup.sh
+++ native/fedora/27/lvm-setup.sh
@@ -1,10 +1,13 @@
 #!/bin/sh
 
-sudo umount /var/reserve/volumes &&
-    sudo umount /var/reserve/system &&
+sudo umount /var/reserve/albums &&
+    sudo umount /var/reserve/structure &&
     sudo rm -rf /var/reserve &&
     TEMP_FSTAB=$(mktemp $(pwd)/XXXXXXXX) &&
     sudo grep -v "/var/reserve" /etc/fstab > ${TEMP_FSTAB} &&
     cat ${TEMP_FSTAB} | sudo tee /etc/fstab &&
-    sudo lvremove --force docker/var_reserve &&
+    sudo lvremove --force albums/var_reserve_albums &&
+    sudo lvremove --force structure/var_reserve_structure &&
+    sudo lvcreate --size 8G --name containers structure &&
+    sudo lvcreate --size 8G --name overlay2 structure &&
     true
