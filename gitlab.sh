b92db8fb3cf919ff309c95fd56f7c0cf751fa409
Emory Merryman <emory.merryman@gmail.com>
2018-06-15 18:48:22 -0400

    try to solve gitlab web ide

--- gitlab.sh
+++ gitlab.sh
@@ -5,10 +5,11 @@ mkdir -p ${HOME}/srv/transient/gitlab/config ${HOME}/srv/transient/gitlab/logs $
 	docker \
 	container \
 	create \
+	--hostname gitlab \
 	--name gitlab \
-	--publish 127.0.1.100:20022:22 \
-	--publish 127.0.1.100:80:80 \
-	--publish 127.0.1.100:443:443 \
+	--publish gitlab:20022:22 \
+	--publish gitlab:80:80 \
+	--publish gitlab:443:443 \
 	--mount type=bind,source=${HOME}/srv/transient/gitlab/config,destination=/etc/gitlab \
 	--mount type=bind,source=${HOME}/srv/transient/gitlab/logs,destination=/var/log/gitlab \
 	--mount type=bind,source=${HOME}/srv/transient/gitlab/data,destination=/var/opt/gitlab \
