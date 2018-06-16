cd5ce6f24c7ea533b4069b303863e58c992a4b27
Emory Merryman <emory.merryman@gmail.com>
2018-06-15 22:25:49 -0400

    remove pam

--- gitlab.sh
+++ gitlab.sh
@@ -15,10 +15,13 @@ mkdir -p ${HOME}/srv/transient/gitlab/config ${HOME}/srv/transient/gitlab/logs $
 	--mount type=bind,source=${HOME}/srv/transient/gitlab/data,destination=/var/opt/gitlab \
 	--mount type=bind,source=${HOME}/srv/permanent/gitlab/backups/application,destination=/var/opt/gitlab/backups \
 	gitlab/gitlab-ce:latest &&
-    docker container start gitlab &&
-    sleep 10m &&
+    docker container start gi tlab &&
+    seq 0 9 | while read I
+    do
+	sudo docker container inspect --format "${I} -- {{.State.Health.Status}}" gitlab &&
+	    sleep 1m
+    done &&
     [ "healthy" == $(docker container inspect --format "{{.State.Health.Status}}" gitlab) ] &&
-    echo WE ESCAPED THE LOOP &&
     BACKUP2=$(sudo ls -1t ${HOME}/srv/permanent/gitlab/backups/application | head -n 1) &&
     if [ ! -z "${BACKUP2}" ]
     then
@@ -32,7 +35,9 @@ mkdir -p ${HOME}/srv/transient/gitlab/config ${HOME}/srv/transient/gitlab/logs $
     docker container exec --interactive --tty gitlab apt-get install --assume-yes cron &&
     FILE=$(docker container exec --interactive --tty gitlab mktemp) &&
     echo "* * * * * nice --adjustment 19 gitlab-rake gitlab:backup:create >> /backup.application.log 2>&1" | docker container exec --interactive gitlab crontab - &&
-    docker container exec --detach gitlab crontab -f &&
+    docker container exec --interactive --tty gitlab sed -i "s@^session    required     pam_loginuid.so\$@#session    required   pam_loginuid.so@" /etc/pam.d/cron
+    docker container exec --detach gitlab cron -f &&
+    sudo ls -1 ${HOME}/srv/permanent/gitlab/backups/application &&
     sleep 1m &&
     sudo ls -1 ${HOME}/srv/permanent/gitlab/backups/application &&
     sleep 1m &&
