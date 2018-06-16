05b4c140687d76e16117a282d7c7476824ea23bf
Emory Merryman <emory.merryman@gmail.com>
2018-06-15 20:55:16 -0400

    do backups not backup

--- gitlab.sh
+++ gitlab.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-mkdir -p ${HOME}/srv/transient/gitlab/config ${HOME}/srv/transient/gitlab/logs ${HOME}/srv/transient/gitlab/data ${HOME}/srv/permanent/gitlab/backup/application ${HOME}/srv/permanent/gitlab/backup/secrets &&
+mkdir -p ${HOME}/srv/transient/gitlab/config ${HOME}/srv/transient/gitlab/logs ${HOME}/srv/transient/gitlab/data ${HOME}/srv/permanent/gitlab/backups/application ${HOME}/srv/permanent/gitlab/backups/secrets &&
     sudo \
 	docker \
 	container \
@@ -13,14 +13,14 @@ mkdir -p ${HOME}/srv/transient/gitlab/config ${HOME}/srv/transient/gitlab/logs $
 	--mount type=bind,source=${HOME}/srv/transient/gitlab/config,destination=/etc/gitlab \
 	--mount type=bind,source=${HOME}/srv/transient/gitlab/logs,destination=/var/log/gitlab \
 	--mount type=bind,source=${HOME}/srv/transient/gitlab/data,destination=/var/opt/gitlab \
-	--mount type=bind,source=${HOME}/srv/permanent/gitlab/backup/application,destination=/var/opt/gitlab/backup \
+	--mount type=bind,source=${HOME}/srv/permanent/gitlab/backups/application,destination=/var/opt/gitlab/backups \
 	gitlab/gitlab-ce:latest &&
     docker container start gitlab &&
     EVENT_FILE=$(mktemp) &&
     sleep 5m &&
     [ "healthy" == $(docker container inspect --format "{{.State.Health.Status}}" gitlab) ] &&
     echo WE ESCAPED THE LOOP &&
-    BACKUP2=$(sudo ls -1t ${HOME}/srv/permanent/gitlab/backup/application | head -n 1) &&
+    BACKUP2=$(sudo ls -1t ${HOME}/srv/permanent/gitlab/backups/application | head -n 1) &&
     if [ ! -z "${BACKUP2}" ]
     then
 	BACKUP1=${BACKUP2%_*} &&
@@ -35,7 +35,7 @@ mkdir -p ${HOME}/srv/transient/gitlab/config ${HOME}/srv/transient/gitlab/logs $
     echo "* * * * * nice --adjustment 19 gitlab-rake gitlab:backup:create >> /backup.application.log 2>&1" | docker container exec --interactive gitlab crontab - &&
     docker container exec --detach gitlab crontab -f &&
     sleep 1m &&
-    sudo ls -1 ${HOME}/srv/permanent/gitlab/backup/application &&
+    sudo ls -1 ${HOME}/srv/permanent/gitlab/backups/application &&
     sleep 1m &&
-    sudo ls -1 ${HOME}/srv/permanent/gitlab/backup/application &&
+    sudo ls -1 ${HOME}/srv/permanent/gitlab/backups/application &&
     echo DONE
