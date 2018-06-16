4e370b764dece278eadd256afda435fd774b3a5b
Emory Merryman <emory.merryman@gmail.com>
2018-06-16 12:00:51 -0400

    try use REGISTRATION_TOKEN

--- gitlab.sh
+++ gitlab.sh
@@ -1,12 +1,14 @@
 #!/bin/sh
 
-mkdir -p ${HOME}/srv/transient/gitlab/config ${HOME}/srv/transient/gitlab/logs ${HOME}/srv/transient/gitlab/data ${HOME}/srv/permanent/gitlab/backups/application ${HOME}/srv/permanent/gitlab/backups/secrets ${HOME}/srv/transient/gitlab-runner/config &&
+export RUNNER_REGISTRATION_TOKEN=$(uuidgen) &&
+    mkdir -p ${HOME}/srv/transient/gitlab/config ${HOME}/srv/transient/gitlab/logs ${HOME}/srv/transient/gitlab/data ${HOME}/srv/permanent/gitlab/backups/application ${HOME}/srv/permanent/gitlab/backups/secrets ${HOME}/srv/transient/gitlab-runner/config &&
     sudo \
 	docker \
 	container \
 	create \
 	--hostname gitlab \
 	--name gitlab \
+	--env GITLAB_OMNIBUS_CONFIG="initial_shared_runners_registration_token=${RUNNER_REGISTRATION_TOKEN}" \
 	--publish 127.0.1.101:20022:22 \
 	--publish 127.0.1.101:80:80 \
 	--publish 127.0.1.101:443:443 \
@@ -21,6 +23,7 @@ mkdir -p ${HOME}/srv/transient/gitlab/config ${HOME}/srv/transient/gitlab/logs $
 	--name gitlab-runner \
 	--mount type=bind,source=${HOME}/srv/transient/gitlab-runner/config,destination=/etc/gitlab-runner \
 	--mount type=bind,source=/var/run/docker.sock,destination=/var/run/docker.sock,readonly=true \
+	--env REGISTRATION_TOKEN=${RUNNER_REGISTRATION_TOKEN} \
 	gitlab/gitlab-runner:latest &&
     docker container start gitlab gitlab-runner &&
     seq 0 9 | while read I
@@ -34,15 +37,22 @@ mkdir -p ${HOME}/srv/transient/gitlab/config ${HOME}/srv/transient/gitlab/logs $
     then
 	BACKUP1=${BACKUP2%_*} &&
 	    BACKUP=${BACKUP1%_*} &&
-	    echo Restoring ${BACKUP} &&
-	    docker container exec --interactive --tty gitlab gitlab-rake gitlab:backup:restore BACKUP=${BACKUP}
+	    echo Restoring BACKUP2=${BACKUP2} BACKUP1=${BACKUP1} BACKUP=${BACKUP} &&
+	    docker container exec --interactive --tty gitlab gitlab-rake gitlab:backup:restore BACKUP=${BACKUP} &&
+	    echo ALPHA 001
     fi &&
+    echo BETA 001 &&
     docker container exec --interactive --tty gitlab apt-get update --assume-yes &&
+    echo BETA 002 &&
     docker container exec --interactive --tty gitlab apt-get install --assume-yes cron &&
-    FILE=$(docker container exec --interactive --tty gitlab mktemp) &&
+    echo BETA 003 &&
     echo "* * * * * nice --adjustment 19 gitlab-rake gitlab:backup:create >> /backup.application.log 2>&1" | docker container exec --interactive gitlab crontab - &&
-    docker container exec --interactive --tty gitlab sed -i "s@^session    required     pam_loginuid.so\$@#session    required   pam_loginuid.so@" /etc/pam.d/cron
+    echo BETA 004 &&
+    docker container exec --interactive --tty gitlab sed -i "s@^session    required 
+    pam_loginuid.so\$@#session    required   pam_loginuid.so@" /etc/pam.d/cron
+    echo BETA 005 &&
     docker container exec --detach gitlab cron -f &&
+    echo BETA 006 &&
     sudo ls -1 ${HOME}/srv/permanent/gitlab/backups/application &&
     sleep 1m &&
     sudo ls -1 ${HOME}/srv/permanent/gitlab/backups/application &&
@@ -50,4 +60,14 @@ mkdir -p ${HOME}/srv/transient/gitlab/config ${HOME}/srv/transient/gitlab/logs $
     sudo ls -1 ${HOME}/srv/permanent/gitlab/backups/application &&
     [ "healthy" == $(docker container inspect --format "{{.State.Health.Status}}" gitlab-runner) ] &&
     docker container exec --interactive --tty gitlab-runner bash &&
+    docker \
+	container \
+	exec \
+	--interactive \
+	--tty \
+	gitlab-runner \
+	gitlab-runner \
+	register \
+	--non-interactive \
+	&&
     echo DONE
