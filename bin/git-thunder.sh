de79a915f9efa00778e3b9248eda3ba2aa0427aa
Emory Merryman <emory.merryman@gmail.com>
2018-06-18 17:32:55 -0400

    repo

--- bin/git-thunder.sh
+++ bin/git-thunder.sh
@@ -6,9 +6,10 @@ git_thunder() {
 	case ${1} in
 	    repository)
 		shift &&
-		    thunder_repository "${@}" &&
+		    git_thunder_repository "${@}" &&
 		    shift ${#}
-	    ;;
+		;;
+	    
 	    *)
 		echo Unknown Option &&
 		    echo ${0} &&
@@ -22,8 +23,10 @@ git_thunder() {
 	while [ ${#} -gt 0 ]
 	do
 	    case ${1} in
-		error)
-		    exit 99
+		organization)
+		    shift &&
+			git_thunder_repository_organization "${@}" &&
+			shift ${#}
 		    ;;
 		*)
 		    echo Unknown Option &&
@@ -33,5 +36,110 @@ git_thunder() {
 		    ;;
 	    esac
 	done
+    } &&
+    git_thunder_repository_organization(){
+	while [ ${#} -gt 0 ]
+	do
+	    case ${1} in
+		create)
+		    shift &&
+			git_thunder_repository_organization_create "${@}" &&
+			shift ${#}
+		    ;;
+		list)
+		    shift &&
+			git_thunder_repository_organization_list "${@}" &&
+			shift ${#}
+		    ;;
+		remove)
+		    shift &&
+			git_thunder_repository_organization_remove "${@}" &&
+			shift ${#}
+		    ;;
+		*)
+		    echo Unknown Option &&
+			echo ${0} &&
+			echo ${@} &&
+			exit 64
+		    ;;
+	    esac
+	done
+    } &&
+    git_thunder_repository_organization_create(){
+	while [ ${#} -gt 0 ]
+	do
+	    case ${1} in
+		--name)
+		    NAME="${2}" &&
+			shift 2
+		    ;;
+		*)
+		    echo Unknown Option &&
+			echo ${0} &&
+			echo ${@} &&
+			exit 64
+		    ;;
+	    esac
+	done &&
+	    if [ -z "${NAME}" ]
+	    then
+		echo Unspecified organization NAME &&
+		    exit 65
+	    fi &&
+	    mkdir -p ${HOME}/srv/reposititories &&
+	    if [ -d ${HOME}/srv/repositories/${NAME} ]
+	    then
+		echo The organization - ${NAME} - already exists. &&
+		    exit 66
+	    fi &&
+	    mkdir ${HOME}/srv/repositories/${NAME}
+    } &&
+    git_thunder_repository_organization_list(){
+	while [ ${#} -gt 0 ]
+	do
+	    case ${1} in
+		*)
+		    echo Unknown Option &&
+			echo ${0} &&
+			echo ${@} &&
+			exit 64
+		    ;;
+	    esac
+	done &&
+	    mkdir -p ${HOME}/srv/reposititories &&
+	    ls -1 ${HOME}/srv/repositories
+    } &&
+    git_thunder_repository_organization_remove(){
+	while [ ${#} -gt 0 ]
+	do
+	    case ${1} in
+		--name)
+		    NAME="${2}" &&
+			shift 2
+		    ;;
+		--force)
+		    FORCE=--force &&
+			shift
+		    ;;
+		*)
+		    echo Unknown Option &&
+			echo ${0} &&
+			echo ${@} &&
+			exit 64
+		    ;;
+	    esac
+	done &&
+	    if [ -z "${NAME}" ]
+	    then
+		echo Unspecified organization NAME &&
+		    exit 65
+	    fi &&
+	    mkdir -p ${HOME}/srv/reposititories &&
+	    if [ ! -d ${HOME}/srv/repositories/${NAME} ]
+	    then
+		echo The organization - ${NAME} - does not exist. &&
+		    exit 66
+	    fi &&
+	    rm --recursive ${FORCE} ${HOME}/srv/repositories/${NAME}
     } &&
     git_thunder "${@}"
