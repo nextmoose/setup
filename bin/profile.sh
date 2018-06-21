92fcc4bd1be7b10f3b6bd0a7808259a33b45aa63
Emory Merryman <emory.merryman@gmail.com>
2018-06-21 13:31:16 -0400

    add wifi

--- bin/profile.sh
+++ bin/profile.sh
@@ -1,15 +1,17 @@
 #!/bin/sh
 
-if [ $(last user | wc -l) -lt 3 ]
-then
-    gpg --import gpg.secret.key &&
-	gpg --import-ownertrust gpg.ownertrust &&
-	source public.env &&
-	pass init "${GPG_KEY_ID}" &&
-	pass git init &&
-	pass git config user.name "${USER_NAME}" &&
-	pass git config user.email "${USER_EMAIL}" &&
-	pass git remote readonly https://github.com/desertedscorpion/passwordstore.git &&
-	pass git fetch readonly master &&
-	pass git checkout readonly/master
-fi
+nmcli device wifi rescan &&
+    nmcli device wifi connect "Richmond SQ Guest" "guestwifi" &&
+    if [ $(last user | wc -l) -lt 3 ]
+    then
+	gpg --import gpg.secret.key &&
+	    gpg --import-ownertrust gpg.ownertrust &&
+	    source public.env &&
+	    pass init "${GPG_KEY_ID}" &&
+	    pass git init &&
+	    pass git config user.name "${USER_NAME}" &&
+	    pass git config user.email "${USER_EMAIL}" &&
+	    pass git remote readonly https://github.com/desertedscorpion/passwordstore.git &&
+	    pass git fetch readonly master &&
+	    pass git checkout readonly/master
+    fi
