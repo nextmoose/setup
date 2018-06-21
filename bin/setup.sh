a659a9c0a45f773936c59fb9f003942981bd80b1
Emory Merryman <emory.merryman@gmail.com>
2018-06-21 12:34:25 -0400

    do it

--- bin/setup.sh
+++ bin/setup.sh
@@ -2,4 +2,10 @@
 
 gpg --import private/gpg.secret.key &&
     gpg --import-ownertrust gpg.ownertrust &&
-    pass git init &&
+    source public.env &&
+    pass git init "${GPG_KEY_ID}" &&
+    pass git config user.name "${USER_NAME}" &&
+    pass git config user.email "${USER_EMAIL}" &&
+    pass git remote add readonly https://github.com/desertedscorpion/passwordstore.git &&
+    pass git fetch readonly master &&
+    pass git checkout master
