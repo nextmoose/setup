54dc221c7b61dca28666fa0d7b79515c01f1b15b
Emory Merryman <emory.merryman@gmail.com>
2018-06-23 11:10:29 -0400

    emacs with packages

--- configuration.d/emacs.nix
+++ configuration.d/emacs.nix
@@ -11,21 +11,21 @@
     { boot.tmpOnTmpfs = true;
 
 let
-  myEmacs = pkgs.emacs; 2
-  emacsWithPackages = (pkgs.emacsPackagesNgGen myEmacs).emacsWithPackages; 3
+  myEmacs = pkgs.emacs;
+  emacsWithPackages = (pkgs.emacsPackagesNgGen myEmacs).emacsWithPackages;
 in
-  emacsWithPackages (epkgs: (with epkgs.melpaStablePackages; [ 4
+  emacsWithPackages (epkgs: (with epkgs.melpaStablePackages; [
     magit          # ; Integrate git <C-x g>
     zerodark-theme # ; Nicolas' theme
-  ]) ++ (with epkgs.melpaPackages; [ 5
+  ]) ++ (with epkgs.melpaPackages; [
     undo-tree      # ; <C-x u> to show the undo tree
     zoom-frm       # ; increase/decrease font size for all buffers %lt;C-x C-+>
-  ]) ++ (with epkgs.elpaPackages; [ 6
+  ]) ++ (with epkgs.elpaPackages; [
     auctex         # ; LaTeX mode
     beacon         # ; highlight my cursor when scrolling
     nameless       # ; hide current package name everywhere in elisp code
   ]) ++ [
-    pkgs.notmuch   # From main packages set 7
+    pkgs.notmuch   # From main packages set
   ])
 
       networking.nameservers = [ hostAddr ];
