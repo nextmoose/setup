bc716f5cc7a50f0c238c97c1e2948879717f09f2
Emory Merryman <emory.merryman@gmail.com>
2018-07-23 08:11:41 -0400

    add breaker

--- docker/images/conductor/root/images/base/root/primary/run.root.secondary.sh
+++ docker/images/conductor/root/images/base/root/primary/run.root.secondary.sh
@@ -23,9 +23,9 @@ mkdir /opt/system/tertiary &&
 sudo /opt/system/tertiary/sbin/${SCRIPT} "\${@}"
 EOF
 		) &&
-				chmod 0555 /opt/system/secondary/scripts/bin/${SCRIPT%.*} &&
+				chmod 0555 /opt/system/tertiary//bin/${SCRIPT%.*} &&
 				cp /opt/system/secondary/scripts/sbin/${SCRIPT} /opt/system/tertiary/sbin/${SCRIPT} &&
-				chmod 0500 /opt/system/secondary/scripts/sbin/${SCRIPT} &&
+				chmod 0500 /opt/system/tertiary/scripts/sbin/${SCRIPT} &&
 				echo "user ALL=(ALL) NOPASSWD:SETENV /opt/system/tertiary/sbin/${SCRIPT}" > /opt/system/tertiary/sudo/${SCRIPT%.*} &&
 				chmod 0444 /opt/system/tertiary/sbin/${SCRIPT%.*} &&
 				true
