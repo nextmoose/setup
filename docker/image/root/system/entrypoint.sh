67f5c04cf646698eb2b58989a53f93cdd4c7fcfd
Emory Merryman <>
2018-08-07 17:57:05 +0000

    try gitlab

--- docker/image/root/system/entrypoint.sh
+++ docker/image/root/system/entrypoint.sh
@@ -7,6 +7,10 @@ TIMESTAMP=$(date +%s) &&
 			echo ${KEY_FILE}
 	} &&
 	WORKSPACE_VOLUME=$(docker volume create --driver lvm --opt thinpool --opt size=1G) &&
+	GITLAB_CONFIG_VOLUME=$(docker volume create --driver lvm --opt thinpool --opt size=1G) &&
+	GITLAB_LOGS_VOLUME=$(docker volume create --driver lvm --opt thinpool --opt size=1G) &&
+	GITLAB_DATA_VOLUME=$(docker volume create --driver lvm --opt thinpool --opt size=1G) &&
+	export MAIN_NETWORK=$(docker network create $(uuidgen) --label timestamp=${TIMESTAMP}) &&
 	docker \
 		container \
 		create \
@@ -14,6 +18,7 @@ TIMESTAMP=$(date +%s) &&
 		--mount type=bind,source=/tmp/.X11-unix,destination=/tmp/.X11-unix,readonly=true \
 		--env DISPLAY \
 		--privileged \
+		--shm-size 256m \
 		--label timestamp=${TIMESTAMP} \
 		urgemerge/chromium-pulseaudio@sha256:21d8120ff7857afb0c18d4abf098549de169782e652437441c3c7778a755e46f \
 		&&
@@ -36,12 +41,23 @@ TIMESTAMP=$(date +%s) &&
 		--env SECRETS_HOST \
 		--env SECRETS_ORGANIZATION \
 		--env SECRETS_REPOSITORY \
+		--env MAIN_NETWORK \
 		--label timestamp=${TIMESTAMP} \
 		rebelplutonium/inner:1.0.13 \
 		&&
-	MAIN=$(docker network create $(uuidgen) --label timestamp=${TIMESTAMP}) &&
-	docker network connect ${MAIN} $(cat browser.cid) &&
-	docker network connect --alias inner ${MAIN} $(cat inner.cid) &&
+	docker \
+		container \
+		create \
+		--cidfile gitlab.cid \
+		--volume ${GITLAB_CONFIG_VOLUME}:/etc/gitlab \
+		--volume ${GITLAB_LOGS_VOLUME}:/var/log/gitlab \
+		--volume ${GITLAB_DATA_VOLUME}:/var/opt/gitlab \
+		gitlab/gitlab-ce:11.1.4-ce.0 \
+		&&
+	docker network connect ${MAIN_NETWORK} $(cat browser.cid) &&
+	docker network connect --alias inner ${MAIN_NETWORK} $(cat inner.cid) &&
+	docker network connect --alias gitlab ${MAIN_NETWORK} $(cat gitlab.cid) &&
 	docker network disconnect bridge $(cat inner.cid) &&
-	docker container start $(cat browser.cid) $(cat inner.cid) &&
+	docker network disconnect bridge $(cat gitlab.cid) &&
+	docker container start $(cat browser.cid) $(cat inner.cid) $(cat gitlab.cid) &&
 	true
