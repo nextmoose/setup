559626dad8abf935c175b83443c6772e226dc00b
Emory Merryman <emory.merryman@gmail.com>
2018-08-20 14:48:11 -0400

    do it

--- bin/example.sh
+++ bin/example.sh
@@ -8,12 +8,14 @@ github \
     --upstream-repository setup \
     --upstream-branch master \
     --upstream-id-rsa upstream.id_rsa \
+    --upstream-known-hosts upstream.known_hosts \
     --origin-host github.com \
     --origin-port 22 \
     --origin-user git \
     --origin-organization nextmoose \
     --origin-repository setup \
     --origin-id-rsa origin.id_rsa \
+    --origin-known-hosts origin.known_hosts \
     --origin-branch nixos-5 \
     --committer-name "Emory Merryman" \
     --committer-email emory.merryman@gmail.com &
