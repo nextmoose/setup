ee8cb0383cc99266b98bbb220bc3cb0818fe2972
Emory Merryman <emory.merryman@gmail.com>
2018-08-22 18:11:49 -0400

    add a mysecreteditor script

---
+++ bin/mysecret-editor.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+secret-editor \
+    --gpg-secret-key gpg.secret.key \
+    --gpg-owner-trust gpg.owner.trust \
+    --gpg2-secret-key gpg.secret.key \
+    --gpg2-owner-trust gpg.owner.trust \
+    --origin-organization desertedscorpion \
+    --origin-repository passwordstore \
+    --origin-id-rsa origin.id_rsa \
+    --origin-known-hosts origin.id_hosts \
+    --committer-name "Emory Merryman" \
+    --committer-email "emory.merryman@gmail.com"
