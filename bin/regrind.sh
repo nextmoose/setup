31e581b25b85cdd6358e6319f7768cf076518d16
Emory Merryman <emory.merryman@gmail.com>
2018-08-23 13:25:13 -0400

    fix regrind script

--- bin/regrind.sh
+++ bin/regrind.sh
@@ -4,5 +4,8 @@ DIR=$(mktemp -d) &&
     sudo mount /dev/sdb1 ${DIR} &&
     cd ${DIR}/nixos/public &&
     git fetch origin nixos-5 &&
+    git checkout -b scratch/$(uuidgen) &&
+    git commit -am "wtf" &&
+    git diff origin/nixos-5 &&
     git checkout origin/nixos-5 &&
     git log -n 1
