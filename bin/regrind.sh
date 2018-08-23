cf726ee6104bb9d2df1e23f6f439ef57f51ee504
Emory Merryman <emory.merryman@gmail.com>
2018-08-23 07:42:06 -0400

    regrind should pull latest log for easier verification

--- bin/regrind.sh
+++ bin/regrind.sh
@@ -4,4 +4,5 @@ DIR=$(mktemp -d) &&
     sudo mount /dev/sdb1 ${DIR} &&
     cd ${DIR}/nixos/public &&
     git fetch origin nixos-5 &&
-    git checkout origin/nixos-5
+    git checkout origin/nixos-5 &&
+    git log -n 1
