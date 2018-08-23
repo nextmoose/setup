083d26050387e882282e7d7797d5ee9902e86f86
Emory Merryman <emory.merryman@gmail.com>
2018-08-23 16:57:31 -0400

    shitty

--- configuration/custom/browser.nix
+++ configuration/custom/browser.nix
@@ -14,19 +14,6 @@ $ ./result/bin/emacs
 */
 { pkgs ? import <nixpkgs> {} }:
 
-let
-  myEmacs = pkgs.emacs;
-  emacsWithPackages = (pkgs.emacsPackagesNgGen myEmacs).emacsWithPackages;
-in
-  emacsWithPackages (epkgs: (with epkgs.melpaStablePackages; [
-    magit          # ; Integrate git <C-x g>
-    zerodark-theme # ; Nicolas' theme
-  ]) ++ (with epkgs.melpaPackages; [
-    undo-tree      # ; <C-x u> to show the undo tree
-  ]) ++ (with epkgs.elpaPackages; [
-    auctex         # ; LaTeX mode
-    beacon         # ; highlight my cursor when scrolling
-    nameless       # ; hide current package name everywhere in elisp code
-  ]) ++ [
-    pkgs.notmuch   # From main packages set
-  ])
+pkgs.chromium.override{
+  name = "shitty";
+}
