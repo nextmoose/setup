6e1874c143ed363115a44103a8ce950c807a013b
Emory Merryman <emory.merryman@gmail.com>
2018-09-01 12:43:00 -0400

    before test local rebuild

--- configuration/custom/experiment.nix
+++ configuration/custom/experiment.nix
@@ -1,4 +1,18 @@
-{stdenv}:
-stdenv.mkDerivation {
-  src=./src;
-}
+/*
+This is a nix expression to build Emacs and some Emacs packages I like
+from source on any distribution where Nix is installed. This will install
+all the dependencies from the nixpkgs repository and build the binary files
+without interfering with the host distribution.
+
+To build the project, type the following from the current directory:
+
+$ nix-build emacs.nix
+
+To run the newly compiled executable:
+
+$ ./result/bin/emacs
+*/
+{ pkgs ? import <nixpkgs> {} }:
+
+pkgs.chromium.mkChromiumDerivation ([
+])
