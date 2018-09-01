3e4a2154f021b4ad676f126d8a2c2dd294f2e632
Emory Merryman <emory.merryman@gmail.com>
2018-09-01 11:43:15 -0400

    before test local rebuild

--- configuration/custom/experiment.nix
+++ configuration/custom/experiment.nix
@@ -1,4 +1,16 @@
-{stdenv}:
-stdenv.mkDerivation {
-  src=./src;
+with import <nixpkgs> {};
+
+let
+  # The ${...} is for string interpolation
+  # The '' quotes are used for multi-line strings
+  simplePackage = pkgs.writeShellScriptBin "whatIsMyIp" ''
+    ${pkgs.curl}/bin/curl http://httpbin.org/get \
+      | ${pkgs.jq}/bin/jq --raw-output .origin
+  '';
+in
+stdenv.mkDerivation rec {
+  name = "test-environment";
+
+  buildInputs = [ simplePackage ];
 }
+
