ba8907045a6e52fb101d5c3206866bfb299e96b5
Emory Merryman <emory.merryman@gmail.com>
2018-09-03 10:54:47 -0400

    simpler

--- configuration/custom/experiment.nix
+++ configuration/custom/experiment.nix
@@ -3,4 +3,10 @@ with import <nixpkgs> {};
 stdenv.mkDerivation {
   name = "hello-2.1.1";
   src = ./src;
+  installPhase = ''
+  mkdir $out &&
+  	mkdir $out/bin &&
+	cp hello.sh $out/bin/hello &&
+	chmod 0555 $out/bin/hello
+  '';
 }
