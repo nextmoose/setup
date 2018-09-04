ca9da137633787fbfc4e38c30dcd0ab2d381756c
Emory Merryman <emory.merryman@gmail.com>
2018-09-04 10:51:02 -0400

    adding myshell

---
+++ configuration/custom/myshell/default.nix
@@ -0,0 +1,13 @@
+{ pkgs ? import <nixpkgs> {} }:
+
+with import <nixpkgs> {};
+
+stdenv.mkDerivation {
+  src = ./src;
+  installPhase = ''
+  mkdir $out &&
+  	mkdir $out/bin &&
+	cp shell.sh $out/bin/myshell &&
+	chmod 0555 $out/bin/myshell
+  '';
+}
