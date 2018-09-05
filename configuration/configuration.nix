1dc2c35c70ff651392e030b1213648877e711ae5
Emory Merryman <emory.merryman@gmail.com>
2018-09-05 10:53:53 -0400

    before test local rebuild

--- configuration/configuration.nix
+++ configuration/configuration.nix
@@ -2,8 +2,46 @@
 {
   imports = [
     ./hardware-configuration.nix
-    ./containers/experiment.nix
   ];
+
+  containers = {
+    experiment = {
+      bindMounts = {
+        "/home/user/host" = {
+	  hostPath = "/";
+	  isReadOnly = true;
+	};
+	"/tmp/.X11-unix" = {
+	  hostPath = "/tmp/.X11-unix";
+	  isReadOnly = true;
+	};
+      };
+      config = { config, pkgs, ... }:
+      {
+        security.sudo.wheelNeedsPassword = false;
+	users.mutableUsers = false;
+	programs.bash = {
+	  shellInit = ''
+	    export DISPLAY=:0
+	  '';
+	  loginShellInit = ''
+	  '';
+	};
+	users.extraUsers.user = {
+	  name = "user" ;
+	  group = "user" ;
+	  extraGroups = [ "wheel" ] ;
+	  home = "/home/user" ;
+	  shell = pkgs.bash ;
+	  hashedPassword = "$6$MBLQmkIrZvB$2bTHy346qybhFBsefUkcFWUrpjJaggoPaHgLksxY5pkdY0k0/NpzIiJEGhLfrsT0F3351UEl2BjU.rNxPzmEl." ;
+	  packages = [
+	    pkgs.firefox
+	    pkgs.emacs
+	  ]
+	};
+      };
+    };
+  };
 
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
