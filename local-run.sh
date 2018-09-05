c80bde2c97e51362b3b91e89adc0898551f455f7
Emory Merryman <emory.merryman@gmail.com>
2018-09-05 11:10:04 -0400

    before test local rebuild

--- local-run.sh
+++ local-run.sh
@@ -1,11 +1,33 @@
 #!/bin/sh
 
-CURRENT_BRANCH=$(git rev-parse --abbrev-ref HEAD) &&
+while [ ${#} -gt 0 ]
+do
+    case ${1} in
+	--password)
+	    export PASSWORD_HASH="$(echo ${2} | mkpasswd --stdin -m sha-512)" &&
+		shift 2
+	;;
+	*)
+	    echo Unknown Option &&
+		echo ${1} &&
+		echo ${0} &&
+		echo ${@} &&
+		exit 64
+	    ;;
+    esac
+done &&
+    if [ -z "${PASSWORD_HASH}" ]
+    then
+	PASSWORD_HASH=$(mkpasswd -m sha-512)
+    fi &&
+    CURRENT_BRANCH=$(git rev-parse --abbrev-ref HEAD) &&
     cleanup() {
 	git checkout ${CURRENT_BRANCH} &&
 	    sudo rm -rf /etc/nixos/custom &&
 	    sudo rm -rf /etc/nixos/containers &&
-	    sudo cp -r configuration/. /etc/nixos &&
+	    sudo sed -e "s#\${PASSWORD_HASH}#${PASSWORD_HASH}#" -e "w/etc/nixos/configuration.nix" configuration/configuration.nix &&
+	    sudo cp -r configuration/containers /etc/nixos &&
+	    sudo cp -r configuration/custom /etc/nixos &&
 	    for CONTAINER in $(sudo nixos-container list)
 	    do
 		sudo nixos-container stop ${CONTAINER}
@@ -22,7 +44,9 @@ CURRENT_BRANCH=$(git rev-parse --abbrev-ref HEAD) &&
     git commit -am "before test local rebuild" --allow-empty &&
     sudo rm -rf /etc/nixos/custom &&
     sudo rm -rf /etc/nixos/containers &&
-    sudo cp -r configuration/. /etc/nixos &&
+    sudo sed -e "s#\${PASSWORD_HASH}#${PASSWORD_HASH}#" -e "w/etc/nixos/configuration.nix" configuration/configuration.nix &&
+    sudo cp -r configuration/containers /etc/nixos &&
+    sudo cp -r configuration/custom /etc/nixos &&
     for CONTAINER in $(sudo nixos-container list)
     do
 	sudo nixos-container stop ${CONTAINER}
