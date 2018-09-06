5f7cdb6bb214b030fab394d7dd07200850a6463c
Emory Merryman <emory.merryman@gmail.com>
2018-09-06 18:03:30 -0400

    wtf

--- local-run.sh
+++ local-run.sh
@@ -3,30 +3,23 @@
 CURRENT_BRANCH=$(git rev-parse --abbrev-ref HEAD) &&
     cleanup() {
 	git checkout ${CURRENT_BRANCH} &&
-	    sudo sh ./run.sh --source configuration --destination /etc/nixos &&
-	    sudo cp configuration/containers.nix /etc/nixos &&
-	    sudo cp -r configuration/containers /etc/nixos &&
-	    sudo cp -r configuration/custom /etc/nixos &&
+	    sudo sh ./run.sh --source configuration --destination /etc/nixos --password password &&
 	    for CONTAINER in $(sudo nixos-container list)
 	    do
-		sudo nixos-container stop ${CONTAINER}
+        	sudo nixos-container stop ${CONTAINER}
 	    done &&
 	    sudo nixos-rebuild switch &&
 	    for CONTAINER in $(sudo nixos-container list)
 	    do
 		sudo nixos-container start ${CONTAINER}
-	    done
+	    done &&
+	    true
     } &&
     trap cleanup EXIT &&
     TEST_BRANCH=scratch/$(uuidgen) &&
     git checkout -b ${TEST_BRANCH} &&
     git commit -am "before test local rebuild" --allow-empty &&
-    sudo rm -rf /etc/nixos/custom &&
-    sudo rm -rf /etc/nixos/containers &&
-    sudo sed -e "s#\${PASSWORD_HASH}#${PASSWORD_HASH}#" -e "w/etc/nixos/configuration.nix" configuration/configuration.nix &&
-    sudo cp -r configuration/containers /etc/nixos &&
-    sudo cp -r configuration/containers /etc/nixos &&
-    sudo cp -r configuration/custom /etc/nixos &&
+    sudo sh ./run.sh --source configuration --destination /etc/nixos --password password &&
     for CONTAINER in $(sudo nixos-container list)
     do
 	sudo nixos-container stop ${CONTAINER}
--- run.sh
+++ run.sh
@@ -11,6 +11,12 @@ do
 	    export DESTINATION="${2}" &&
 		shift 2
 	    ;;
+	--user-
+	
+	password)
+		 export HASHED_USER_PASSWORD=$(uuidgen | mkpasswd --stdin -m sha-512) &&
+		     shift 2
+		 ;;
 	*)
 	    echo Unknown Option &&
 		echo ${1} &&
@@ -29,7 +35,6 @@ done &&
 	echo Unspecified DESTINATION &&
 	    exit 66
     fi &&
-    HASHED_USER_PASSWORD=$(uuidgen | mkpasswd -m sha-512) &&
     rm -rf ${DESTINATION}/configuration.nix ${DESTINATION}/containers.nix ${DESTINATION}/containers ${DESTINATION}/custom &&
     sed -e "s#\${PASSWORD_HASH}#${HASHED_USER_PASSWORD}#" -e "w${DESTINATION}/configuration.nix" ${SOURCE}/configuration.nix &&
     cp -r ${SOURCE}/containers.nix ${SOURCE}/containers ${SOURCE}/custom ${DESTINATION}
