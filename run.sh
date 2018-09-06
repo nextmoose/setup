f7ee322daa3e30b38134f6df35ff376ba0f1beb7
Emory Merryman <emory.merryman@gmail.com>
2018-09-06 11:10:59 -0400

    before test local rebuild

---
+++ run.sh
@@ -0,0 +1,34 @@
+#!/bin/sh
+
+while [ ${#} -gt 0 ]
+do
+    case ${1} in
+	--source)
+	    export SOURCE="${2}" &&
+		shift 2
+	    ;;
+	--destination)
+	    export DESTINATION="${2}" &&
+		shift 2
+	    ;;
+	*)
+	    echo Unknown Option &&
+		echo ${1} &&
+		echo ${0} &&
+		echo ${@} &&
+		exit 64
+	    ;;
+    esac
+done &&
+    if [ -z "${SOURCE}" ]
+    then
+	echo Unspecifed SOURCE &&
+	    exit 65
+    elif [ -z "${DESTINATION}" ]
+    then
+	echo Unspecified DESTINATION &&
+	    exit 66
+    fi &&
+    HASHED_USER_PASSWORD=$(uuidgen | mkpasswd -m sha-512)
+    sudo sed -e "s#\${PASSWORD_HASH}#${HASHED_USER_PASSWORD}#" -e "w${DESTINATION}/configuration.nix" ${SOURCE}/configuration.nix &&
+    sudo cp -r ${SOURCE}/containers.nix ${SOURCE}/containers ${SOURCE}/custom ${DESTINATION}
