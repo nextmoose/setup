4af7f4b24e057e83e42279843e445dff97452cc2
Emory Merryman <emory.merryman@gmail.com>
2018-09-07 09:14:15 -0400

    before test local rebuild

---
+++ configuration/custom/migration/example/default.nix
@@ -0,0 +1,14 @@
+{ pkgs ? import <nixpkgs> {} }:
+
+with import <nixpkgs> {};
+
+stdenv.mkDerivation {
+  name = "example";
+  src = ./src;
+  installPhase = ''
+  mkdir $out &&
+  	mkdir $out/bin &&
+	cp example.sh $out/bin/myshell &&
+	chmod 0555 $out/bin/example
+  '';
+}
