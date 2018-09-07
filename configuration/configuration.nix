a32d45ac84f24fe46a43b1e4c85ccd77769bc8ce
Emory Merryman <emory.merryman@gmail.com>
2018-09-07 17:19:26 -0400

    before test local rebuild

--- configuration/configuration.nix
+++ configuration/configuration.nix
@@ -11,6 +11,8 @@
   networking.hostName = "duke";
   networking.networkmanager.enable=true;
   networking.networkmanager.unmanaged = [ "interface-name:ve-*" ];
+  networking.nat.enable = true;
+  networking.useHostResolve = true;
 
   i18n = {
     consoleFont = "Lat2-Terminus16";
