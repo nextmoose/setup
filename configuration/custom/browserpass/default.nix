be767e3df8bbe5671568d9c8b836582941c7c3c5
Emory Merryman <emory.merryman@gmail.com>
2018-09-08 16:31:11 -0400

    before test local rebuild

---
+++ configuration/custom/browserpass/default.nix
@@ -0,0 +1,17 @@
+{ pkgs ? import <nixpkgs> {} }:
+
+with import <nixpkgs> {};
+
+stdenv.mkDerivation {
+  name = "browserpass";
+  src = fetchzip {
+    url = "https://github.com/browserpass/browserpass/releases/download/2.0.22/browserpass-linux64.zip";
+    sha256 = "0m0d6x1jhccks43h9qw3rgpvl5l7rb4crmfr4jf1ws7r29ibjbm2";
+    stripRoot = false;
+  };
+  installPhase = ''
+  ls -alh &&
+  echo $out &&
+  pwd
+  '';
+}
