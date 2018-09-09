9faf87fcc1aae04fd3843fc8766647069ee674a2
Emory Merryman <emory.merryman@gmail.com>
2018-09-08 21:06:25 -0400

    before test local rebuild

--- configuration/configuration.nix
+++ configuration/configuration.nix
@@ -70,6 +70,7 @@
     hashedPassword = "${PASSWORD_HASH}";
     packages = [
       (import ./custom/init/default.nix { inherit pkgs; })
+      (import ./custom/my-browser/default.nix { inherit pkgs; })
       (import ./custom/migration/emacs-development-environment/default.nix { inherit pkgs; })
       (import ./custom/migration/emacs-nixpkgs/default.nix { inherit pkgs; })
       (import ./custom/migration/example/default.nix { inherit pkgs; })
