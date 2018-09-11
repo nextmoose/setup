1c921fb513a905513c8f42a039486258d2ccbcc5
Emory Merryman <emory.merryman@gmail.com>
2018-09-11 18:01:20 -0400

    before test local rebuild

--- configuration/custom/shopsafe/default.nix
+++ configuration/custom/shopsafe/default.nix
@@ -5,9 +5,11 @@ with import <nixpkgs> {};
 stdenv.mkDerivation {
   name = "shopsafe";
   src = ./src;
-  buildInputs = [ "docker" ];
   installPhase = ''
-    mkdir $out &&
-      docker image build .
+    pkgs.dockerTools.buildImage {
+      name="hello";
+      tag="001";
+      fromImage="alpine:3.4";
+    };
   '';
 }
