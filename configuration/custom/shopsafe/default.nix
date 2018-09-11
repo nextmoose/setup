4b5e740eca7f4cdef8f3bd54577f903bab4e30a5
Emory Merryman <emory.merryman@gmail.com>
2018-09-11 18:32:49 -0400

    before test local rebuild

--- configuration/custom/shopsafe/default.nix
+++ configuration/custom/shopsafe/default.nix
@@ -1,13 +1,14 @@
 { pkgs ? import <nixpkgs> {} }:
 
 with import <nixpkgs> {};
-
-stdenv.mkDerivation {
+let
+  image = dockerTools.pullImage {
+    imageName = "ubuntu";
+    imageDigest = "sha256:ce4ea6b8cb8730d6a48b747037d0448b8151ea666d65f58e1b967147b0787734";
+    sha256 = "0na3idj2g4zzg39h0g55wzwy2vpvbblfrw3iw1b1ybaqn6bvs6nz";
+    finalImageTag = "16.04";
+  };
+in dockerTools.buildImage {
   name = "shopsafe";
-  src = ./src;
-  buildInputs = [ "docker" ];
-  installPhase = ''
-    mkdir $out &&
-      docker image build .
-  '';
+  fromImage = image;
 }
