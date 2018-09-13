9a691d0a81305b281c8fa17b09cdd533731b3eeb
Emory Merryman <emory.merryman@gmail.com>
2018-09-13 11:01:14 -0400

    before test local rebuild

--- configuration/custom/browser/default.nix
+++ configuration/custom/browser/default.nix
@@ -10,12 +10,14 @@ stdenv.mkDerivation {
     pkgs.git
     pkgs.pass
     pkgs.gnupg
-    (import ../post-commit/default.nix { inherit pkgs; })
+    pkgs.browserpass
+    (import ../migration/post-commit/default.nix { inherit pkgs; })
+    (import ../migration/gpg-key-id/default.nix { inherit pkgs; })
   ];
   installPhase = ''
     mkdir $out &&
       mkdir $out/bin &&
-      cp my-browser.sh $out/bin/browser &&
+      cp browser.sh $out/bin/browser &&
       chmod 0555 $out/bin/browser
   '';
 }
