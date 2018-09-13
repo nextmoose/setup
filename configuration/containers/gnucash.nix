723a9af29d0c070eef22562f934fadc4571b1d5b
Emory Merryman <emory.merryman@gmail.com>
2018-09-13 19:47:06 -0400

    before test local rebuild

---
+++ configuration/containers/gnucash.nix
@@ -0,0 +1,36 @@
+let
+  foo = "bar";
+in
+{
+  bindMounts = {
+    "/tmp/.X11-unix" = {
+      hostPath = "/tmp/.X11-unix";
+      isReadOnly = true;
+    };
+    "/secrets" = {
+      hostPath = "/secrets";
+      isReadOnly = true;
+    };
+  };
+  config = { config, pkgs, ... }:
+  {
+      environment.variables = {
+        DISPLAY=":0";
+      };
+
+      users.mutableUsers = false;
+
+    users.extraUsers.user = {
+      name = "user" ;
+      group = "users" ;
+      extraGroups = [ "wheel" ] ;
+      shell = pkgs.bash ;
+      createHome = true ;
+      home = "/home/user";
+      hashedPassword = "$6$MBLQmkIrZvB$2bTHy346qybhFBsefUkcFWUrpjJaggoPaHgLksxY5pkdY0k0/NpzIiJEGhLfrsT0F3351UEl2BjU.rNxPzmEl." ;
+      packages = [
+        gnucash
+      ];
+    };
+  };
+}
