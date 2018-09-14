6d7234ac9be52c60e8a09afe16075cab7848c0a8
Emory Merryman <emory.merryman@gmail.com>
2018-09-14 15:29:16 -0400

    before test local rebuild

--- configuration/custom/migration/regrind/src/regrind.sh
+++ configuration/custom/migration/regrind/src/regrind.sh
@@ -5,7 +5,7 @@ DIR=$(mktemp -d) &&
     cd ${DIR}/nixos/public &&
     git fetch origin nixos-5 &&
     git checkout -b scratch/$(uuidgen) &&
-    git commit -am "wtf" &&
+    git commit -am "wtf" --allow-empty &&
     git diff origin/nixos-5 &&
     git checkout origin/nixos-5 &&
     git log -n 1
