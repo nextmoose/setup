6b3b15814ea99e4431ba3a098454511083221089
Emory Merryman <emory.merryman@gmail.com>
2018-09-14 17:21:17 -0400

    before test local rebuild

--- configuration/custom/migration/gpg-key-id/default.nix
+++ configuration/custom/migration/gpg-key-id/default.nix
@@ -1,14 +1,17 @@
 { pkgs ? import <nixpkgs> {} }:
 
 with import <nixpkgs> {};
-
-stdenv.mkDerivation {
-  name = "gpg-key-id";
-  src = ./src;
-  installPhase = ''
-  mkdir $out &&
-  	mkdir $out/bin &&
-	cp gpg-key-id.sh $out/bin/gpg-key-id &&
-	chmod 0555 $out/bin/gpg-key-id
+let
+  script = pkgs.writeShellScriptBin "gpg-key-id" ''
+     ${pkgs.gnupg}/bin/gpg --list-keys --with-colon | head --lines 5 | tail --lines 1 | cut --fields 5 --delimiter ":"
   '';
+in
+stdenv.mkDerivation rec {
+  name = "gpg-key-id";
+  buildInputs = [ pkgs.gnupg script ];
+  installPhase = ''
+    mkdir $out &&
+      mkdir $out/bin
+  '';
+  src = ./src;
 }
