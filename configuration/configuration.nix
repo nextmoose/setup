64940a9aa549557dbc639e269c9186d6991b9242
Emory Merryman <emory.merryman@gmail.com>
2018-09-14 09:49:22 -0400

    before test local rebuild

--- configuration/configuration.nix
+++ configuration/configuration.nix
@@ -4,61 +4,27 @@
     ./hardware-configuration.nix
     ./containers.nix
   ];
-
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
-
   networking.hostName = "duke";
   networking.networkmanager.enable=true;
   networking.networkmanager.unmanaged = [ "interface-name:ve-*" ];
   networking.nat.enable = true;
   networking.nat.internalInterfaces = ["ve-+"];
   networking.nat.externalInterface = "wl01";
-
   i18n = {
     consoleFont = "Lat2-Terminus16";
     consoleKeyMap = "us";
     defaultLocale = "en_US.UTF-8";
   };
-
   time.timeZone = "US/Eastern";
-
-  nixpkgs.config =
-        let
-          plugins = 
-	  {
- 	    enableAdobeFlash = true;
-	    enablePepperPDF = true;
-	    enableGoogleTalkPlugin = true;
-            jre = true;
-          };
-        in
-        { # firefox = plugins;
-          chromium = plugins;
-          allowUnfree = true;
-        };
-
   virtualisation.docker.enable = true;
-
-  environment.systemPackages = with pkgs; [
-    chromium
-    firefoxWrapper
-    git
-    pass
-    gnupg
-    bash-completion
-    nix-bash-completions
-  ];
-
   sound.enable = true;
   hardware.pulseaudio.enable = true;
-
   services.xserver.enable = true;
   services.xserver.windowManager.i3.enable = true;
   services.xserver.libinput.enable = true;
-  
   security.sudo.wheelNeedsPassword = false;
-
   programs.bash = {
     shellInit = ''
       my-completions &&
@@ -69,20 +35,7 @@
       xhost +local:
     '';
   };
-  
-
-  programs.chromium = {
-    enable = true;
-    extensions = [
-      "naepdomgkenhinolocfifgehidddafch"
-    ];
-  };
-  programs.browserpass = {
-    enable = true;
-  };
-
   users.mutableUsers = false;
-  
   users.extraUsers.user = {
     isNormalUser = true;
     uid = 1000;
@@ -110,37 +63,7 @@
       pkgs.nix-bash-completions
       pkgs.xorg.xhost
       pkgs.browserpass
-];
+    ];
   };
-  
-  users.extraUsers.browser = {
-    isNormalUser = true;
-    uid = 1001;
-    extraGroups = [ "wheel" "networkmanager" ];
-    hashedPassword = "${PASSWORD_HASH}";
-    packages = [
-      (import ./custom/init/default.nix { inherit pkgs; })
-      (import ./custom/my-browser/default.nix { inherit pkgs; })
-      (import ./custom/migration/emacs-development-environment/default.nix { inherit pkgs; })
-      (import ./custom/migration/emacs-nixpkgs/default.nix { inherit pkgs; })
-      (import ./custom/migration/example/default.nix { inherit pkgs; })
-      (import ./custom/migration/github/default.nix { inherit pkgs; })
-      (import ./custom/migration/gpg-key-id/default.nix { inherit pkgs; })
-      (import ./custom/migration/init/default.nix { inherit pkgs; })
-      (import ./custom/migration/mysecret-editor/default.nix { inherit pkgs; })
-      (import ./custom/migration/post-commit/default.nix { inherit pkgs; })
-      (import ./custom/migration/pre-push/default.nix { inherit pkgs; })
-      (import ./custom/migration/regrind/default.nix { inherit pkgs; })
-      (import ./custom/migration/secret-editor/default.nix { inherit pkgs; })
-      (import ./custom/my-completions/default.nix { inherit pkgs; })
-      pkgs.emacs
-      pkgs.mkpasswd
-      pkgs.bash-completion
-      pkgs.nix-bash-completions
-      pkgs.xorg.xhost
-      pkgs.browserpass
-];
-  };
-  
   system.stateVersion = "18.03";
 }
