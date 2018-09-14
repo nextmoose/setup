ad950fe4d0d5ab122c672c7bdbaeb4f52cfb4587
Emory Merryman <emory.merryman@gmail.com>
2018-09-14 13:38:23 -0400

    before test local rebuild

--- configuration/custom/browser/default.nix
+++ configuration/custom/browser/default.nix
@@ -1,19 +1,10 @@
 { pkgs ? import <nixpkgs> {} }:
 with import <nixpkgs> {};
+let
+  script = pkgs.writeShellScriptBin "browser" ''
+    ${pkgs.chromium} --disable-gpu
+  '';
 stdenv.mkDerivation {
   name = "browser";
-  src = ./src;
-  installPhase = ''
-    mkdir $out &&
-      mkdir $out/bin &&
-      sed \
-        -e "s#pkgs.git#${pkgs.git}#" \
-        -e "s#pkgs.gnupg#${pkgs.gnupg}#" \
-        -e "s#pkgs.pass#${pkgs.pass}#" \
-        -e "s#pkgs.chromium#${pkgs.chromium}#" \
-	-e "s#pkgs.gpg-key-id#(import ./custom/migration/gpg-key-id/default.nix { inherit pkgs; })#" \
-	-e "w$out/bin/browser" \
-	browser.sh &&
-      chmod 0555 $out/bin/browser
-  '';
+  buildInputs = [ script ];
 }
