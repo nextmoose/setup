5a6bd1fb44fe9eeafc931bbe57646fae3fd02aa7
Emory Merryman <emory.merryman@gmail.com>
2018-09-14 10:26:13 -0400

    before test local rebuild

--- configuration/configuration.nix
+++ configuration/configuration.nix
@@ -27,12 +27,7 @@
   security.sudo.wheelNeedsPassword = false;
   programs.bash = {
     shellInit = ''
-      my-completions &&
-      date >> /tmp/shellInit.log.txt
-    '';
-    loginShellInit = ''
-      date >> /tmp/loginShellInit.log.txt &&
-      xhost +local:
+      init-home
     '';
   };
   users.mutableUsers = false;
@@ -42,7 +37,6 @@
     extraGroups = [ "wheel" "networkmanager" "docker" ];
     hashedPassword = "${PASSWORD_HASH}";
     packages = [
-      (import ./custom/init/default.nix { inherit pkgs; })
       (import ./custom/secrets/default.nix { inherit pkgs; })
       (import ./custom/my-browser/default.nix { inherit pkgs; })
       (import ./custom/migration/emacs-development-environment/default.nix { inherit pkgs; })
