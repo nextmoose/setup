6990242b7fccc5f07e58b99cba428d93f534dce4
Emory Merryman <emory.merryman@gmail.com>
2018-09-14 18:37:29 -0400

    before test local rebuild

--- configuration/custom/migration/gpg-key-id/default.nix
+++ configuration/custom/migration/gpg-key-id/default.nix
@@ -1,3 +1,3 @@
 { pkgs ? import <nixpkgs> {} }:
 with import <nixpkgs> {};
-pkgs.writeShellScriptBin "gpg-key-id" "${pkgs.gnupg}/bin/gpg --list-keys --with-colon | head --lines 5 | tail --lines 1 | cut --fields 5 --delimiter ':'"
+pkgs.writeShellScriptBin "gpg-key-id" "${pkgs.gnupg}/bin/gpg --list-keys --with-colon | head --lines 5 | tail --lines 1 | cut --fields 5 --delimiter 'A'"
