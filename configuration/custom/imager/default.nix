65b388b0303e4adde3f00793ecc492e1641d7260
Emory Merryman <emory.merryman@gmail.com>
2018-09-15 11:15:22 -0400

    before test local rebuild

--- configuration/custom/imager/default.nix
+++ configuration/custom/imager/default.nix
@@ -1,6 +1,8 @@
 { pkgs ? import <nixpkgs> {} }:
 with import <nixpkgs> {};
 pkgs.writeShellScriptBin "imager" ''
-  sudo lvcreate --name image --size 10G volumes &&
-    sudo mount /dev/image /tmp/image
+  cd $(mktemp -d) &&
+    ${pkgs.curl}/bin/curl https://d3g5gsiof5omrk.cloudfront.net/nixos/18.03/nixos-18.03.133224.5f59ab7d4e0/nixos-minimal-18.03.133224.5f59ab7d4e0-x86_64-linux.iso
+    mkdir image &&
+    sudo mount nixos-minimal-18.03.133224.5f59ab7d4e0-x86_64-linux.iso image
 ''
