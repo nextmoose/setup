4de74a67d4c19564f051a94f9e30c15b3fc16d13
Emory Merryman <emory.merryman@gmail.com>
2018-09-16 10:10:20 -0400

    do it

--- createit.sh
+++ createit.sh
@@ -1,3 +1,27 @@
 #!/bin/sh
 
-nix-build '<nixpkgs/nixos>' -A config.system.build.isoImage -I nixos-config=iso.nix
+VBoxManage \
+    createvm \
+    --name nixos \
+    --groups /nixos \
+    --register &&
+    nix-build '<nixpkgs/nixos>' -A config.system.build.isoImage -I nixos-config=iso.nix &&
+    VBoxManage storagectl nixos --name "SATA Controller" --add SATA &&
+    VBoxManage \
+	storageattach \
+	nixos \
+	--storagectl "SATA Controller" \
+	--port 0 \
+	--device 0 \
+	--type dvddrive \
+	--medium $(pwd)/result/iso/nixos-18.03.133098.cd0cd946f37-x86_64-linux.iso &&
+    VBoxManage storagectl nixos --name "IDE" --add IDE &&
+    VBoxManage \
+	storageattach \
+	nixos \
+	--storagectl "IDE" \
+	--port 0 \
+	--device 0 \
+	--type hdd \
+	--medium $(pwd)/result/iso/nixos-18.03.133098.cd0cd946f37-x86_64-linux.iso &&    
+    true
