4b1b784b8f6a47ddc8c109b21cdd8000e994448d
Emory Merryman <emory.merryman@gmail.com>
2018-09-17 10:52:36 -0400

    do it

--- createit.sh
+++ createit.sh
@@ -1,46 +1,48 @@
 #!/bin/sh
 
-cleanup(){
-    VBoxManage list vms | cut --fields 2 --delimiter '"' | while read VM
-    do
+VM=$(uuidgen) &&
+    SSH_KEY=$(mktemp) &&
+    VMDK=$(mktemp) &&
+    cleanup(){
 	VBoxManage controlvm ${VM} poweroff soft &&
 	    sleep 1m &&
-	    VBoxManage unregistervm --delete ${VM}
-    done &&
-	sudo lvremove --force /dev/volumes/nixos &&
-	true
-} &&
+	    VBoxManage unregistervm --delete ${VM} &&
+	    sudo lvremove --force /dev/volumes/${VM} &&
+	    rm -f ${SSH_KEY} &&
+	    true
+    } &&
     trap cleanup EXIT &&
+    ssh-keygen -f ${SSH_KEY} -P "" -C "" &&
     nix-build '<nixpkgs/nixos>' -A config.system.build.isoImage -I nixos-config=iso.nix &&
-    sudo lvcreate --name nixos --size 100GB volumes &&
+    sudo lvcreate --name ${VM} --size 100GB volumes &&
     VBoxManage \
 	internalcommands \
 	createrawvmdk -filename  \
-	$(pwd)/nixos.vmdk \
-	-rawdisk /dev/volumes/nixos &&
+	${VMDK} \
+	-rawdisk /dev/volumes/${VM} &&
     VBoxManage \
 	createvm \
-	--name nixos \
+	--name ${VM} \
 	--groups /nixos \
 	--register &&
     VBoxManage storagectl nixos --name "SATA Controller" --add SATA &&
     VBoxManage \
 	storageattach \
-	nixos \
+	${VM} \
 	--storagectl "SATA Controller" \
 	--port 0 \
 	--device 0 \
 	--type dvddrive \
 	--medium $(pwd)/result/iso/nixos-18.03.133098.cd0cd946f37-x86_64-linux.iso &&
-    VBoxManage storagectl nixos --name "IDE" --add IDE &&
+    VBoxManage storagectl ${VM} --name "IDE" --add IDE &&
     VBoxManage \
 	storageattach \
-	nixos \
+	${VM} \
 	--storagectl "IDE" \
 	--port 0 \
 	--device 0 \
 	--type hdd \
-	--medium $(pwd)/nixos.vmdk &&
+	--medium ${VMDK} &&
     VBoxManage startvm nixos &&
     read -p "IS THE VERSION MACHINE READY?  " READ1 &&
     ssh -i id_rsa -l root 64.137.201.46 hello &&
