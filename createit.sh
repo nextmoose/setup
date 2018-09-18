68262138c8761e18d79206e6863dfc14d6d0ec0c
Emory Merryman <emory.merryman@gmail.com>
2018-09-18 14:54:23 -0400

    use read

--- createit.sh
+++ createit.sh
@@ -70,7 +70,7 @@ VM=nixos-$((${RANDOM}%9000+1000)) &&
     done &&
     echo waited for keyscan &&
     echo finished waiting for vm &&
-    prompt -p READY READY &&
+    read -p READY READY &&
     #    ssh -i ${SSH_KEY} -l root -p ${PORT} -o UserKnownHostsFile=${KNOWN_HOSTS} 127.0.0.1 install &&
     VBoxManage controlvm ${VM} poweroff soft &&
     VBoxManage storageattach ${VM} --storagectl "SATA Controller" --port 0 --device 0 --medium none &&
