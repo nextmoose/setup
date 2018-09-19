bb00bd5da763edd6e59e94e011b379fd1bc5c7cc
Emory Merryman <emory.merryman@gmail.com>
2018-09-19 17:16:48 -0400

    try efi

--- createit.sh
+++ createit.sh
@@ -59,6 +59,7 @@ VM=nixos-$((${RANDOM}%9000+1000)) &&
     VBoxManage modifyvm "${VM}" --natpf1 "guestssh,tcp,127.0.0.1,${PORT},,22" &&
     VBoxManage modifyvm "${VM}" --nic1 nat &&
     VBoxManage modifyvm "${VM}" --memory 2000 &&
+    VBoxManage modifyvm "${VM}" --firmware efi &&
     echo SSH KEY=${SSH_KEY} &&
     echo PORT=${PORT} &&
     VBoxManage startvm ${VM} &&
