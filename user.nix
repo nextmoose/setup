d5709954171ff4758ff211ddae0c3487c8675af2
Emory Merryman <emory.merryman@gmail.com>
2018-09-24 13:35:09 -0400

    do it

--- user.nix
+++ user.nix
@@ -1,11 +1,11 @@
-users.extraUsers.user = {
-  isNormalUser = true;
-  uid = 1000;
-  extraGroups = [ "wheel" ];
-  hashedPassword = "HASHED_PASSWORD";
-  packages = [
-  ];
-  openssh.authorizedKeys.keys = [
-    "AUTHORIZED_KEY_PUBLIC"
-  ];
-};
+{ config, pkgs, ... }:
+{
+  users.extraUsers.user = {
+    isNormalUser = true;
+    uid = 1000;
+    extraGroups = [ "wheel" ];
+    hashedPassword = "HASHED_PASSWORD";
+    packages = [
+    ];
+  };
+}
