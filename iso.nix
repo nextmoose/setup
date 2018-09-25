5386201b73ff21e7c73f9cfb98332ad3431b8f07
Emory Merryman <emory.merryman@gmail.com>
2018-09-24 21:19:50 -0400

    do it

--- iso.nix
+++ iso.nix
@@ -5,11 +5,24 @@
     <nixpkgs/nixos/modules/installer/cd-dvd/channel.nix>
     ./user.nix
   ];
+  i18n = {
+    consoleFont = "Lat2-Terminus16";
+    consoleKeyMap = "us";
+    defaultLocale = "en_US.UTF-8";
+  };
+  time.timeZone = "US/Eastern";
+  virtualisation.docker.enable = true;
+  virtualisation.virtualbox.host.enable = true;
+  users.extraUsers.myuser.extraGroups = ["vboxusers"];
+  sound.enable = true;
+  hardware.pulseaudio.enable = true;
+  services.xserver.enable = true;
+  services.xserver.windowManager.i3.enable = true;
+  services.xserver.libinput.enable = true;
+  security.sudo.wheelNeedsPassword = false;
   environment.systemPackages = [
     (import ./installer/default.nix  { inherit pkgs; })
   ];
   systemd.services.sshd.wantedBy = pkgs.lib.mkForce [ "multi-user.target" ];
-  users.users.root.openssh.authorizedKeys.keys = [
-    "AUTHORIZED_KEY_PUBLIC"
-  ];
+  users.mutableUsers = false;
 }
