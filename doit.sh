394b82f32193dd41ca8be8b45f6e7c9359e97d75
Emory Merryman <emory.merryman@gmail.com>
2018-09-25 16:57:20 -0400

    complete?

--- doit.sh
+++ doit.sh
@@ -71,8 +71,8 @@ EOF
 	    VBoxManage createvm --name nixos --groups /nixos --register &&
 	    VBoxManage storagectl nixos --name "SATA Controller" --add SATA &&
 	    VBoxManage storageattach nixos --storagectl "SATA Controller" --port 0 --device 0 --type dvddrive --medium ${WORK_DIR}/result/iso/nixos-18.03.133098.cd0cd946f37-x86_64-linux.iso &&
-	    VBoxManage storagectl ${VM} --name "IDE" --add IDE &&
-	    VBoxManage storageattach nixos --storagectl "IDE" --port 0 --device 0 --type hdd --medium ${VMDK} &&
+	    VBoxManage storagectl nixos --name "IDE" --add IDE &&
+	    VBoxManage storageattach nixos --storagectl "IDE" --port 0 --device 0 --type hdd --medium ${WORK_DIR}/nixos.vmdk &&
 	    VBoxManage modifyvm nixos --natpf1 "guestssh1,tcp,127.0.0.1,${PORT1},,22" &&
 	    VBoxManage modifyvm nixos --nic1 nat &&
 	    VBoxManage modifyvm nixos --memory 2000 &&
@@ -86,7 +86,63 @@ EOF
 		done
 	    } &&
 	    knownhosts alpha &&
-	    time ssh -F ${WORK_DIR}/.ssh/config alpha installer
+	    time ssh -F ${WORK_DIR}/.ssh/config alpha installer &&
+	    VBoxManage controlvm nixos poweroff soft &&
+	    VBoxManage storageattach nixos --storagectl "SATA Controller" --port 0 --device 0 --medium none &&
+	    VBoxManage startvm --type headless nixos &&
+	    knownhosts gamma &&
+	    testit() {
+		while [ ${#} -gt 0 ]
+		do
+		    case ${1} in
+			--title)
+			    TITLE="${2}" &&
+				shift 2
+			    ;;
+			--expected-exit-code)
+			    EXPECTED_EXIT_CODE="${2}" &&
+				shift 2
+			    ;;
+			--expected-output)
+			    EXPECTED_OUTPUT="${2}" &&
+				shift 2
+			    ;;
+			--command)
+			    COMMAND="${2}" &&
+				shift 2
+			    ;;
+			*)
+			    echo Unexpected Argument &&
+				echo ${@} &&
+				exit 65
+			    ;;
+		    esac
+		done &&
+		    echo TITLE=${TITLE} &&
+		    echo COMMAND=${COMMAND} &&
+		    echo EXPECTED_OUTPUT=${EXPECTED_OUTPUT} &&
+		    echo EXPECTED_EXIT_CODE=${EXPECTED_EXIT_CODE} &&
+		    BEFORE=$(date +%s) &&
+		    echo ssh -i ${SSH_KEY} -l user -p ${PORT1} -o UserKnownHostsFile=${KNOWN_HOSTS2} 127.0.0.1 '${COMMAND}' &&
+		    OBSERVED_OUTPUT="$(ssh -i ${SSH_KEY} -l user -p ${PORT1} -o UserKnownHostsFile=${KNOWN_HOSTS2} 127.0.0.1 \"${COMMAND}\")" &&
+		    OBSERVED_EXIT_CODE=${?} &&
+		    AFTER=$(date +%s) &&
+		    echo DURATION=$((${AFTER}-${BEFORE})) &&
+		    echo OBSERVED_OUTPUT=${OBSERVED_OUTPUT} &&
+		    echo OBSERVED_EXIT_CODE=${OBSERVED_EXIT_CODE} &&
+		    if [ "${OBSERVED_EXIT_CODE}" != "${EXPECTED_EXIT_CODE}" ]
+		    then
+			echo Unexpected exit code &&
+			    exit 66
+		    elif [ "${OBSERVED_OUTPUT}" != "${EXPECTED_OUTPUT}" ]
+		    then
+			echo Unexpected output &&
+			    exit 67
+		    else
+			echo SUCCESS
+		    fi
+	    } &&
+	    testit --title "We have a secrets program." --expected-output hello --expected-exit-code 0 --command secrets
     ) &&
     STATUS=PASS &&
     true
