59a846a44a457d73d173fe70a3dbf02b02bd07e3
Emory Merryman <emory.merryman@gmail.com>
2018-09-26 12:03:08 -0400

    add it to path

--- custom/secrets/default.nix
+++ custom/secrets/default.nix
@@ -4,11 +4,10 @@ with import <nixpkgs> {};
 stdenv.mkDerivation {
   name = "secrets";
   src = ./src;
-  buildInputs = [ pkgs.gnupg ];
   installPhase = ''
     mkdir $out &&
       mkdir $out/bin &&
-      sed -e "s#OUT#$out#" -e "w$out/bin/secrets" secrets.sh &&
+      sed -e "s#OUT#$out#" -e "s#GNUPG#{pkgs.gnupg}" -e "w$out/bin/secrets" secrets.sh &&
       chmod 0555 $out/bin/secrets &&
       mkdir $out/etc &&
       cp -r secrets $out/etc
