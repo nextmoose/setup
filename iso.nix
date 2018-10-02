d4efd6c8e5a1a093f1eca3259d353cc646ab7251
Emory Merryman <emory.merryman@gmail.com>
2018-10-02 08:06:47 -0400

    this should... work

--- iso.nix
+++ iso.nix
@@ -3,9 +3,12 @@
   imports = [
     <nixpkgs/nixos/modules/installer/cd-dvd/installation-cd-minimal.nix>
     <nixpkgs/nixos/modules/installer/cd-dvd/channel.nix>
-    ./iso-ssh.nix
   ];
   environment.systemPackages = [
-    (import ./custom/my-install/default.nix  { inherit pkgs; })
+    (import ./installer/default.nix  { inherit pkgs; })
+  ];
+  systemd.services.sshd.wantedBy = pkgs.lib.mkForce [ "multi-user.target" ];
+  users.users.root.openssh.authorizedKeys.keys = [
+    "AUTHORIZED_KEY_PUBLIC"
   ];
 }
