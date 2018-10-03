004046daa7a15098ef6e8520fdab7434baa2f205
Emory Merryman <emory.merryman@gmail.com>
2018-10-02 20:19:28 -0400

    do it

---
+++ custom/wifi/default.nix
@@ -0,0 +1,8 @@
+with import <nixpkgs> {};
+let
+ myScript = pkgs.writeShellScriptBin "wifi" "${pkgs.networkmanager}/bin/nmcli device wifi connect "Richmond Sq Guest" password "guestwifi"";
+in
+stdenv.mkDerivation rec {
+  name = "wifi";
+  buildInputs = [ myScript ];
+}
