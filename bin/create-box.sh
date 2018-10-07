8dda1e3566f3e374d2a317e5c9a33c8069432197
Emory Merryman <emory.merryman@gmail.com>
2018-10-07 10:53:58 -0400

    created

--- bin/create-box.sh
+++ bin/create-box.sh
@@ -16,15 +16,15 @@ fi &&
     then
 	sudo VBoxManage internalcommands createrawvmdk -filename build/box/nixos.vmdk -rawdisk /dev/volumes/nixos
     fi &&
-    VBoxManage createvm --name nixos --groups /nixos --register &&
-    VBoxManage storagectl nixos --name "SATA Controller" --add SATA &&
-    VBoxManage storageattach nixos --storagectl "SATA Controller" --port 0 --device 0 --type dvddrive --medium build/virtual/result/iso/$(ls -1 build/virtual/result/iso) &&
-    VBoxManage storagectl nixos --name "IDE" --add IDE &&
-    VBoxManage storageattach nixos --storagectl "IDE" --port 0 --device 0 --type hdd --medium build/box/nixos.vmdk &&
-    VBoxManage modifyvm nixos --natpf1 "install,tcp,127.0.0.1,20560,,22" &&
-    VBoxManage modifyvm nixos --natpf1 "use,tcp,127.0.0.1,29156,,22" &&
-    VBoxManage modifyvm nixos --nic1 nat &&
-    VBoxManage modifyvm nixos --nic2 bridged &&
-    VBoxManage modifyvm nixos --bridgeadapter2 wlo1 &&
-    VBoxManage modifyvm nixos --memory 2000 &&
-    VBoxManage modifyvm nixos --firmware efi
+    sudo VBoxManage createvm --name nixos --groups /nixos --register &&
+    sudo VBoxManage storagectl nixos --name "SATA Controller" --add SATA &&
+    sudo VBoxManage storageattach nixos --storagectl "SATA Controller" --port 0 --device 0 --type dvddrive --medium build/virtual/result/iso/$(ls -1 build/virtual/result/iso) &&
+    sudo VBoxManage storagectl nixos --name "IDE" --add IDE &&
+    sudo VBoxManage storageattach nixos --storagectl "IDE" --port 0 --device 0 --type hdd --medium build/box/nixos.vmdk &&
+    sudo VBoxManage modifyvm nixos --natpf1 "install,tcp,127.0.0.1,20560,,22" &&
+    sudo VBoxManage modifyvm nixos --natpf1 "use,tcp,127.0.0.1,29156,,22" &&
+    sudo VBoxManage modifyvm nixos --nic1 nat &&
+    sudo VBoxManage modifyvm nixos --nic2 bridged &&
+    sudo VBoxManage modifyvm nixos --bridgeadapter2 wlo1 &&
+    sudo VBoxManage modifyvm nixos --memory 2000 &&
+    sudo VBoxManage modifyvm nixos --firmware efi
