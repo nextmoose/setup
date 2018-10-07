1c803b028d033c4125d2d0cbe9f0df925498cf4e
Emory Merryman <emory.merryman@gmail.com>
2018-10-07 12:26:05 -0400

    store luks.txt in encrypted secrets directory

--- bin/virtual-build.sh
+++ bin/virtual-build.sh
@@ -5,4 +5,6 @@ sh $(dirname ${0})/create-virtual.sh &&
     scp -F build/dot-ssh/config build/virtual/installer/src/configuration.nix use:/etc/nixos/configuration.nix &&
     scp --recursive -F build/dot-ssh/config build/virtual/installer/src/custom use:/etc/nixos/custom &&
     ssh -F build/dot-ssh/config nixos-rebuild switch &&
+    sudo VBoxManage controlvm nixos poweroff soft &&
+    sh $(dirname ${0})/boot.sh &&
     true
