d0a0cd505b63c1360bdc2b6ce70e5eb6ce86c381
Emory Merryman <emory.merryman@gmail.com>
2018-10-07 11:54:10 -0400

    do it

--- bin/virtual-build.sh
+++ bin/virtual-build.sh
@@ -1,4 +1,8 @@
 #!/bin/sh
 
 sh $(dirname ${0})/create-virtual.sh &&
-    s
+    scp -F build/dot-ssh/config build/virtual/installer/src/configuration.isolated.nix use:/etc/nixos/configuration.isolated.nix &&
+    scp -F build/dot-ssh/config build/virtual/installer/src/configuration.nix use:/etc/nixos/configuration.nix &&
+    scp --recursive -F build/dot-ssh/config build/virtual/installer/src/custom use:/etc/nixos/custom &&
+    ssh -F build/dot-ssh/config nixos-rebuild switch &&
+    true
