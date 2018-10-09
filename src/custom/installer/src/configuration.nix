0dbab3b5baaead61b7cd1429266450d34dd6449d
Emory Merryman <emory.merryman@gmail.com>
2018-10-09 11:04:12 -0400

    try again

--- src/custom/installer/src/configuration.nix
+++ src/custom/installer/src/configuration.nix
@@ -6,32 +6,32 @@
   ];
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
-  networking = {
-    networkmanager = {
-      enable = true;
-      unmanaged = [ "interface-name:ve-*" ];
-    };
-    nat = {
-      enable = true;
-      internalInterfaces = [ "ve-+" ];
-      externalInterface = "wl01";
-    };
-  };
-  i18n = {
-    consoleFont = "Lat2-Terminus16";
-    consoleKeyMap = "us";
-    defaultLocale = "en_US.UTF-8";
-  };
-  time.timeZone = "US/Eastern";
-  virtualisation.docker.enable = true;
-  virtualisation.virtualbox.host.enable = true;
-  services.xserver = {
-    enable = true;
-    windowManager.i3.enable = true;
-    libinput.enable = true;
-  };
-  sound.enable = true;
-  hardware.pulseaudio.enable = true;
+#  networking = {
+#    networkmanager = {
+#      enable = true;
+#      unmanaged = [ "interface-name:ve-*" ];
+#    };
+#    nat = {
+#      enable = true;
+#      internalInterfaces = [ "ve-+" ];
+#      externalInterface = "wl01";
+#    };
+#  };
+#  i18n = {
+#    consoleFont = "Lat2-Terminus16";
+#    consoleKeyMap = "us";
+#    defaultLocale = "en_US.UTF-8";
+#  };
+#  time.timeZone = "US/Eastern";
+#  virtualisation.docker.enable = true;
+#  virtualisation.virtualbox.host.enable = true;
+#  services.xserver = {
+#    enable = true;
+#    windowManager.i3.enable = true;
+#    libinput.enable = true;
+#  };
+#  sound.enable = true;
+#  hardware.pulseaudio.enable = true;
   security.sudo.wheelNeedsPassword = false;
   users.mutableUsers = false;
   users.extraUsers.user.isNormalUser = true;
