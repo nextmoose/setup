b11dfde76c337bc3eb28b9758e80301cd01d0730
Emory Merryman <emory.merryman@gmail.com>
2018-10-08 22:41:38 -0400

    wtf

--- bin/create-install-iso.sh
+++ bin/create-install-iso.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
-sh $(dirname ${0})/create-install-src.sh &&
+sh $(dirname ${0})/destroy-pass-gpg-tar.sh &&
+    sh $(dirname ${0})/create-pass-gpg-tar.sh &&
+    sh $(dirname ${0})/create-install-src.sh &&
     cd build/src &&
     nix-build '<nixpkgs/nixos>' -A config.system.build.isoImage -I nixos-config=iso.nix
