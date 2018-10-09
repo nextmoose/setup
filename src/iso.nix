f9810430fd2e6e739e12a50c51d5d35eb59ebc23
Emory Merryman <emory.merryman@gmail.com>
2018-10-08 20:56:40 -0400

    do it

--- src/iso.nix
+++ src/iso.nix
@@ -1,10 +1,14 @@
 {config, pkgs, ...}:
+let installer = (import ./custom/installer/default.nix { inherit pkgs; });
 {
   imports = [
     <nixpkgs/nixos/modules/installer/cd-dvd/installation-cd-minimal.nix>
     <nixpkgs/nixos/modules/installer/cd-dvd/channel.nix>
   ];
+  programs.bash.shellInit = ''
+    ${installer}/bin/installer
+  '';
   environment.systemPackages = [
-    (import ./custom/installer/default.nix { inherit pkgs; })
+    ${installer}
   ];
 }
