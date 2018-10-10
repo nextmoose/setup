b4bbba13de76d963ffdc95248c9221579783559d
Emory Merryman <emory.merryman@gmail.com>
2018-10-10 17:40:28 -0400

    this might work, but the images are incredibly big and take forever to build

--- src/iso.nix
+++ src/iso.nix
@@ -4,7 +4,9 @@
     <nixpkgs/nixos/modules/installer/cd-dvd/installation-cd-minimal.nix>
     <nixpkgs/nixos/modules/installer/cd-dvd/channel.nix>
   ];
+  isoImage.includeSystemBuildDependencies = true;
   environment.systemPackages = [
+    pkgs.sudo
     pkgs.mkpasswd
     (import ./custom/installer/default.nix { inherit pkgs; })
     pkgs.chromium
@@ -15,6 +17,7 @@
     pkgs.mkpasswd
   ];
   system.extraDependencies = [
+    pkgs.sudo
     pkgs.chromium
     pkgs.emacs
     pkgs.gnupg
