81f3d79bbd5e256d419b80e7fbc09d19aec6ba79
Emory Merryman <emory.merryman@gmail.com>
2018-10-11 18:24:19 -0400

    remove config

--- bin/create-virtual-machine.sh
+++ bin/create-virtual-machine.sh
@@ -7,15 +7,7 @@ if [ ! -d build ]
     sh $(dirname ${0})/destroy-virtual-machine.sh &&
     sh $(dirname ${0})/create-install-iso.sh &&
     sudo lvcreate -y --name nixos --size 100GB volumes &&
-    sudo lvcreate -y --name config --size 1G volumes &&
-    echo y | sudo mkfs.ext4 /dev/volumes/config &&
-    if [ ! -d build/config ]
-    then
-	mkdir build/config &&
-	    sudo mount /dev/volumes/config build/config
-    fi &&
     sudo VBoxManage internalcommands createrawvmdk -filename build/nixos.vmdk -rawdisk /dev/volumes/nixos &&
-    sudo VBoxManage internalcommands createrawvmdk -filename build/config.vmdk -rawdisk /dev/volumes/config &&
     sudo VBoxManage createvm --name nixos --groups /nixos --register &&
     sudo VBoxManage storagectl nixos --name "SATA Controller" --add SATA &&
     ls -1 build/src/result/iso | while read FILE
@@ -24,7 +16,6 @@ if [ ! -d build ]
     done &&
     sudo VBoxManage storagectl nixos --name "IDE" --add IDE &&
     sudo VBoxManage storageattach nixos --storagectl "IDE" --port 0 --device 0 --type hdd --medium build/nixos.vmdk &&
-    sudo VBoxManage storageattach nixos --storagectl "IDE" --port 0 --device 1 --type hdd --medium build/config.vmdk &&
     sudo VBoxManage modifyvm nixos --memory 2000 &&
     sudo VBoxManage modifyvm nixos --firmware efi &&
     true
--- bin/destroy-virtual-machine.sh
+++ bin/destroy-virtual-machine.sh
@@ -17,15 +17,6 @@ fi &&
     then
 	sudo lvremove --force /dev/volumes/nixos
     fi &&
-    if [ "0" != "$(sudo lvs | grep -c config)" ]
-    then
-	if [ -d build/config ]
-	then
-	    sudo umount build/config &&
-		rm --recursive --force build/config
-	fi &&
-	sudo lvremove --force /dev/volumes/config
-    fi &&
     rm --force build/nixos.vmdk &&
     rm --recursive --force build/src &&
     rm --recursive --force build &&
