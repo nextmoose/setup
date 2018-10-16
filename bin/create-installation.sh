a818e128578a2a7cc36fbd1b04befbc35099a4b8
Emory Merryman <emory.merryman@gmail.com>
2018-10-16 09:32:38 -0400

    build the iso image

--- bin/create-installation.sh
+++ bin/create-installation.sh
@@ -5,4 +5,6 @@ sh $(dirname ${0})/create-secrets.sh &&
     mkdir build/installation &&
     cp --recursive build/configuration/. build/installation &&
     cp build/root.tar.gpg build/installation/custom/installer/src &&
+    cd build/installation &&
+    nix-build '<nixpkgs/nixos>' -A config.system.build.isoImage -I nixos-config=iso.nix
     true
