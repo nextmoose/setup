c987a782f0205a92ea3c6770f671f70541b6b0bd
Emory Merryman <emory.merryman@gmail.com>
2018-10-16 12:16:18 -0400

    i think I attached the iso image but (1) I have not verified it (2) it is hard coded

--- src/testing/installation/test.nix
+++ src/testing/installation/test.nix
@@ -2,11 +2,17 @@ import <nixpkgs/nixos/tests/make-test.nix> {
   machine =
     { config, pkgs, ... }:
     {
+       config.virtualisation.diskImage = "./nixos.qcow2";
+       config.virtualisation.emptyDiskImages = [ 1000 1000 1000 ];
+       config.virtualisation.qemu.options = [
+        "-cdrom /nix/store/4xlkspgrqhygm7byflvadwd5rdknph5s-nixos-18.03.133245.d16a7abceb7-x86_64-linux.iso/iso/nixos-18.03.133245.d16a7abceb7-x86_64-linux.iso"
+      ];
     };
   testScript =
     ''
-      $machine->start() &&
-        $machine->waitForUnit("default.target");
-        $machine->shutdown()
+      $machine->start();
+      $machine->waitForUnit("default.target");
+      $machine->succeed("ls -lah /dev");
+      $machine->shutdown();
     '';
 }
