52709e6e3c840ee175e114d0b791bc2d141f3443
Emory Merryman <emory.merryman@gmail.com>
2018-10-16 15:41:17 -0400

    use virtual box

--- src/testing/installation/test.nix
+++ src/testing/installation/test.nix
@@ -2,17 +2,17 @@ import <nixpkgs/nixos/tests/make-test.nix> {
   machine =
     { config, pkgs, ... }:
     {
-       config.virtualisation.diskImage = "./nixos.qcow2";
-       config.virtualisation.emptyDiskImages = [ 1000 1000 1000 ];
-       config.virtualisation.qemu.options = [
-        "-cdrom /nix/store/4xlkspgrqhygm7byflvadwd5rdknph5s-nixos-18.03.133245.d16a7abceb7-x86_64-linux.iso/iso/nixos-18.03.133245.d16a7abceb7-x86_64-linux.iso"
+      config.virtualisation.diskImage = "/tmp/nixos-18.03.133245.d16a7abceb7-x86_64-linux.iso";
+      config.virtualisation.qemu.options = [
+        "-cdrom /tmp/nixos-18.03.133245.d16a7abceb7-x86_64-linux.iso"
       ];
+      config.virtualisation.emptyDiskImages = [ 10000 10000 10000 ];
     };
   testScript =
     ''
       $machine->start();
-      $machine->waitForUnit("default.target");
-      $machine->succeed("ls -lah /dev");
+      $machine->execute("sleep 20s");
+      $machine->screenshot("20s");
       $machine->shutdown();
     '';
 }
