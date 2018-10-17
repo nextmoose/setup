9259f8804b9e988cc4fa9688ecfc6f6d29615f27
Emory Merryman <emory.merryman@gmail.com>
2018-10-17 01:30:22 -0400

    add shutdown command

--- bin/boot-machine.sh
+++ bin/boot-machine.sh
@@ -1,8 +1,16 @@
 #!/bin/sh
 
 read -s -p "LUKS PASSPHRASE? " LUKS_PASSPHRASE &&
+    echo &&
+    read -s -p "USER PASSWORD? " USER_PASSWORD &&
     sudo VBoxManage startvm nixos &&
-    sleep 60s &&
+    sleep 30s &&
     echo "${LUKS_PASSPHRASE}" | sh $(dirname ${0})/transcribe-keyboard-alpha.sh &&
+    sh $(dirname ${0})/type-keyboard-enter.sh &&
+    sleep 30s &&
+    echo user | sh $(dirname ${0})/transcribe-keyboard-alpha.sh &&
+    sh $(dirname ${0})/type-keyboard-enter.sh &&
+    sleep 20s &&
+    echo "${USER_PASSWORD}" | sh $(dirname ${0})/transcribe-keyboard-alpha.sh &&
     sh $(dirname ${0})/type-keyboard-enter.sh &&
     true
---
+++ bin/shutdown-machine.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+
+sudo VBoxManage controlvm nixos poweroff soft &&
+    true
