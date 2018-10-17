5d1d5ad84c0a9787441544bed81f49c57e1aa429
Emory Merryman <emory.merryman@gmail.com>
2018-10-16 22:36:44 -0400

    no guarantee

--- bin/transcribe-keyboard-alpha.sh
+++ bin/transcribe-keyboard-alpha.sh
@@ -8,196 +8,196 @@ PRESS=-- &&
     do
 	case ${SYMBOL} in
 	    "a")
-		sh $(dirname ${0})/type-keyboard-key --key "a"
+		sh $(dirname ${0})/type-keyboard-key.sh --key "a"
 		;;
 	    "b")
-		sh $(dirname ${0})/type-keyboard-key --key "b"
+		sh $(dirname ${0})/type-keyboard-key.sh --key "b"
 		;;
 	    "c")
-		sh $(dirname ${0})/type-keyboard-key --key "c"
+		sh $(dirname ${0})/type-keyboard-key.sh --key "c"
 		;;
 	    "d")
-		sh $(dirname ${0})/type-keyboard-key --key "d"
+		sh $(dirname ${0})/type-keyboard-key.sh --key "d"
 		;;
 	    "e")
-		sh $(dirname ${0})/type-keyboard-key --key "e"
+		sh $(dirname ${0})/type-keyboard-key.sh --key "e"
 		;;
 	    "f")
-		sh $(dirname ${0})/type-keyboard-key --key "f"
+		sh $(dirname ${0})/type-keyboard-key.sh --key "f"
 		;;
 	    "g")
-		sh $(dirname ${0})/type-keyboard-key --key "g"
+		sh $(dirname ${0})/type-keyboard-key.sh --key "g"
 		;;
 	    "h")
-		sh $(dirname ${0})/type-keyboard-key --key "h"
+		sh $(dirname ${0})/type-keyboard-key.sh --key "h"
 		;;
 	    "i")
-		sh $(dirname ${0})/type-keyboard-key --key "i"
+		sh $(dirname ${0})/type-keyboard-key.sh --key "i"
 		;;
 	    "j")
-		sh $(dirname ${0})/type-keyboard-key --key "j"
+		sh $(dirname ${0})/type-keyboard-key.sh --key "j"
 		;;
 	    "k")
-		sh $(dirname ${0})/type-keyboard-key --key "k"
+		sh $(dirname ${0})/type-keyboard-key.sh --key "k"
 		;;
 	    "l")
-		sh $(dirname ${0})/type-keyboard-key --key "l"
+		sh $(dirname ${0})/type-keyboard-key.sh --key "l"
 		;;
 	    "m")
-		sh $(dirname ${0})/type-keyboard-key --key "m"
+		sh $(dirname ${0})/type-keyboard-key.sh --key "m"
 		;;
 	    "n")
-		sh $(dirname ${0})/type-keyboard-key --key "n"
+		sh $(dirname ${0})/type-keyboard-key.sh --key "n"
 		;;
 	    "o")
-		sh $(dirname ${0})/type-keyboard-key --key "o"
+		sh $(dirname ${0})/type-keyboard-key.sh --key "o"
 		;;
 	    "p")
-		sh $(dirname ${0})/type-keyboard-key --key "p"
+		sh $(dirname ${0})/type-keyboard-key.sh --key "p"
 		;;
 	    "q")
-		sh $(dirname ${0})/type-keyboard-key --key "q"
+		sh $(dirname ${0})/type-keyboard-key.sh --key "q"
 		;;
 	    "r")
-		sh $(dirname ${0})/type-keyboard-key --key "r"
+		sh $(dirname ${0})/type-keyboard-key.sh --key "r"
 		;;
 	    "s")
-		sh $(dirname ${0})/type-keyboard-key --key "s"
+		sh $(dirname ${0})/type-keyboard-key.sh --key "s"
 		;;
 	    "t")
-		sh $(dirname ${0})/type-keyboard-key --key "t"
+		sh $(dirname ${0})/type-keyboard-key.sh --key "t"
 		;;
 	    "u")
-		sh $(dirname ${0})/type-keyboard-key --key "u"
+		sh $(dirname ${0})/type-keyboard-key.sh --key "u"
 		;;
 	    "v")
-		sh $(dirname ${0})/type-keyboard-key --key "v"
+		sh $(dirname ${0})/type-keyboard-key.sh --key "v"
 		;;
 	    "w")
-		sh $(dirname ${0})/type-keyboard-key --key "w"
+		sh $(dirname ${0})/type-keyboard-key.sh --key "w"
 		;;
 	    "x")
-		sh $(dirname ${0})/type-keyboard-key --key "x"
+		sh $(dirname ${0})/type-keyboard-key.sh --key "x"
 		;;
 	    "y")
-		sh $(dirname ${0})/type-keyboard-key --key "y"
+		sh $(dirname ${0})/type-keyboard-key.sh --key "y"
 		;;
 	    "z")
-		sh $(dirname ${0})/type-keyboard-key --key "z"
+		sh $(dirname ${0})/type-keyboard-key.sh --key "z"
 		;;
 	    "A")
-		sh $(dirname ${0})/type-keyboard-key --key "a" --case upper
+		sh $(dirname ${0})/type-keyboard-key.sh --key "a" --case upper
 		;;
 	    "B")
-		sh $(dirname ${0})/type-keyboard-key --key "b" --case upper
+		sh $(dirname ${0})/type-keyboard-key.sh --key "b" --case upper
 		;;
 	    "C")
-		sh $(dirname ${0})/type-keyboard-key --key "c" --case upper
+		sh $(dirname ${0})/type-keyboard-key.sh --key "c" --case upper
 		;;
 	    "D")
-		sh $(dirname ${0})/type-keyboard-key --key "d" --case upper
+		sh $(dirname ${0})/type-keyboard-key.sh --key "d" --case upper
 		;;
 	    "E")
-		sh $(dirname ${0})/type-keyboard-key --key "e" --case upper
+		sh $(dirname ${0})/type-keyboard-key.sh --key "e" --case upper
 		;;
 	    "F")
-		sh $(dirname ${0})/type-keyboard-key --key "f" --case upper
+		sh $(dirname ${0})/type-keyboard-key.sh --key "f" --case upper
 		;;
 	    "G")
-		sh $(dirname ${0})/type-keyboard-key --key "g" --case upper
+		sh $(dirname ${0})/type-keyboard-key.sh --key "g" --case upper
 		;;
 	    "H")
-		sh $(dirname ${0})/type-keyboard-key --key "h" --case upper
+		sh $(dirname ${0})/type-keyboard-key.sh --key "h" --case upper
 		;;
 	    "I")
-		sh $(dirname ${0})/type-keyboard-key --key "i" --case upper
+		sh $(dirname ${0})/type-keyboard-key.sh --key "i" --case upper
 		;;
 	    "J")
-		sh $(dirname ${0})/type-keyboard-key --key "j" --case upper
+		sh $(dirname ${0})/type-keyboard-key.sh --key "j" --case upper
 		;;
 	    "K")
-		sh $(dirname ${0})/type-keyboard-key --key "k" --case upper
+		sh $(dirname ${0})/type-keyboard-key.sh --key "k" --case upper
 		;;
 	    "L")
-		sh $(dirname ${0})/type-keyboard-key --key "l" --case upper
+		sh $(dirname ${0})/type-keyboard-key.sh --key "l" --case upper
 		;;
 	    "M")
-		sh $(dirname ${0})/type-keyboard-key --key "m" --case upper
+		sh $(dirname ${0})/type-keyboard-key.sh --key "m" --case upper
 		;;
 	    "N")
-		sh $(dirname ${0})/type-keyboard-key --key "n" --case upper
+		sh $(dirname ${0})/type-keyboard-key.sh --key "n" --case upper
 		;;
 	    "O")
-		sh $(dirname ${0})/type-keyboard-key --key "o" --case upper
+		sh $(dirname ${0})/type-keyboard-key.sh --key "o" --case upper
 		;;
 	    "P")
-		sh $(dirname ${0})/type-keyboard-key --key "p" --case upper
+		sh $(dirname ${0})/type-keyboard-key.sh --key "p" --case upper
 		;;
 	    "Q")
-		sh $(dirname ${0})/type-keyboard-key --key "q" --case upper
+		sh $(dirname ${0})/type-keyboard-key.sh --key "q" --case upper
 		;;
 	    "R")
-		sh $(dirname ${0})/type-keyboard-key --key "r" --case upper
+		sh $(dirname ${0})/type-keyboard-key.sh --key "r" --case upper
 		;;
 	    "S")
-		sh $(dirname ${0})/type-keyboard-key --key "s" --case upper
+		sh $(dirname ${0})/type-keyboard-key.sh --key "s" --case upper
 		;;
 	    "T")
-		sh $(dirname ${0})/type-keyboard-key --key "t" --case upper
+		sh $(dirname ${0})/type-keyboard-key.sh --key "t" --case upper
 		;;
 	    "U")
-		sh $(dirname ${0})/type-keyboard-key --key "u" --case upper
+		sh $(dirname ${0})/type-keyboard-key.sh --key "u" --case upper
 		;;
 	    "V")
-		sh $(dirname ${0})/type-keyboard-key --key "v" --case upper
+		sh $(dirname ${0})/type-keyboard-key.sh --key "v" --case upper
 		;;
 	    "W")
-		sh $(dirname ${0})/type-keyboard-key --key "w" --case upper
+		sh $(dirname ${0})/type-keyboard-key.sh --key "w" --case upper
 		;;
 	    "X")
-		sh $(dirname ${0})/type-keyboard-key --key "x" --case upper
+		sh $(dirname ${0})/type-keyboard-key.sh --key "x" --case upper
 		;;
 	    "Y")
-		sh $(dirname ${0})/type-keyboard-key --key "y" --case upper
+		sh $(dirname ${0})/type-keyboard-key.sh --key "y" --case upper
 		;;
 	    "Z")
-		sh $(dirname ${0})/type-keyboard-key --key "z" --case upper
+		sh $(dirname ${0})/type-keyboard-key.sh --key "z" --case upper
 		;;
 	    "0")
-		sh $(dirname ${0})/type-keyboard-key --key "0"
+		sh $(dirname ${0})/type-keyboard-key.sh --key "0"
 		;;
 	    "1")
-		sh $(dirname ${0})/type-keyboard-key --key "1"
+		sh $(dirname ${0})/type-keyboard-key.sh --key "1"
 		;;
 	    "2")
-		sh $(dirname ${0})/type-keyboard-key --key "2"
+		sh $(dirname ${0})/type-keyboard-key.sh --key "2"
 		;;
 	    "3")
-		sh $(dirname ${0})/type-keyboard-key --key "3"
+		sh $(dirname ${0})/type-keyboard-key.sh --key "3"
 		;;
 	    "4")
-		sh $(dirname ${0})/type-keyboard-key --key "4"
+		sh $(dirname ${0})/type-keyboard-key.sh --key "4"
 		;;
 	    "5")
-		sh $(dirname ${0})/type-keyboard-key --key "5"
+		sh $(dirname ${0})/type-keyboard-key.sh --key "5"
 		;;
 	    "6")
-		sh $(dirname ${0})/type-keyboard-key --key "6"
+		sh $(dirname ${0})/type-keyboard-key.sh --key "6"
 		;;
 	    "7")
-		sh $(dirname ${0})/type-keyboard-key --key "7"
+		sh $(dirname ${0})/type-keyboard-key.sh --key "7"
 		;;
 	    "8")
-		sh $(dirname ${0})/type-keyboard-key --key "8"
+		sh $(dirname ${0})/type-keyboard-key.sh --key "8"
 		;;
 	    "9")
-		sh $(dirname ${0})/type-keyboard-key --key "9"
+		sh $(dirname ${0})/type-keyboard-key.sh --key "9"
 		;;
 	    "-")
-		sh $(dirname ${0})/type-keyboard-key --key "-"
+		sh $(dirname ${0})/type-keyboard-key.sh --key "-"
 		;;
  	    "|")
-		sh $(dirname ${0})/type-keyboard-key --key "\\"
+		sh $(dirname ${0})/type-keyboard-key.sh --key "\\"
 		;;
 	    *)
 		echo "Unknown Symbol \"${SYMBOL}\"" &&
